8ce3dceb3d92f5e04aabdb0b42cd18c4b2a7bba8
Victor Luchits <vluchits@gmail.com>
2013-09-15 16:08:31 +0400

    Pass --disable-shared to configure script if not building shared libraries

--- tools/cross_compile/libs/curl.sh
+++ tools/cross_compile/libs/curl.sh
@@ -4,6 +4,7 @@ MORE_OPTS=""
 [ ! -z "$HOST" ] && MORE_OPTS="${MORE_OPTS} --host=${HOST}"
 [ ! -z "$DATA_DIR" ] && MORE_OPTS="${MORE_OPTS} --datadir=${DATA_DIR}"
 [ "$ENABLE_SHARED" = "YES" ] && MORE_OPTS="${MORE_OPTS} --enable-shared"
+[ "$ENABLE_SHARED" != "YES" ] && MORE_OPTS="${MORE_OPTS} --disable-shared"
 
 cd ${SOURCE_DIR}libsrcs/libcurl && \
 MORE_OPTS="${MORE_OPTS} --build=`./config.guess`" \
