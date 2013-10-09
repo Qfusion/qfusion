7490f078d71521991086dbf02919d62791494005
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-10-09 20:36:12 +0200

    Fix screenshot formatstring formatting mixup

--- source/ref_gl/r_register.c
+++ source/ref_gl/r_register.c
@@ -713,7 +713,7 @@ static void R_Register( const char *screenshotsPrefix )
 
 	r_screenshot_jpeg = ri.Cvar_Get( "r_screenshot_jpeg", "1", CVAR_ARCHIVE );
 	r_screenshot_jpeg_quality = ri.Cvar_Get( "r_screenshot_jpeg_quality", "90", CVAR_ARCHIVE );
-	r_screenshot_fmtstr = ri.Cvar_Get( "r_screenshot_fmtstr", va( "%s%y%m%d_%H%M%S", screenshotsPrefix ), CVAR_ARCHIVE );
+	r_screenshot_fmtstr = ri.Cvar_Get( "r_screenshot_fmtstr", va( "%s%y%%m%%d_%H%M%%S", screenshotsPrefix ), CVAR_ARCHIVE );
 
 #ifdef GLX_VERSION
 	r_swapinterval = ri.Cvar_Get( "r_swapinterval", "0", CVAR_ARCHIVE|CVAR_LATCH_VIDEO );
