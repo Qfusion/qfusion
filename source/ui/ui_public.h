e0c4fe9b53bec8082eec41c7a453312005a81506
Victor Luchits <vluchits@gmail.com>
2013-11-24 01:36:18 +0400

    Bump UI API version

--- source/ui/ui_public.h
+++ source/ui/ui_public.h
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef __UI_PUBLIC_H__
 #define __UI_PUBLIC_H__
 
-#define	UI_API_VERSION	    42
+#define	UI_API_VERSION	    43
 
 typedef size_t (*ui_async_stream_read_cb_t)(const void *buf, size_t numb, float percentage, 
 	int status, const char *contentType, void *privatep);
