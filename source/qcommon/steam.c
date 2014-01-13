b726816a40fa5fae2fc993a2b3b2e07a4a158580
Victor Luchits <vluchits@gmail.com>
2014-01-13 22:51:51 +0400

    Add Steam wrapper library

---
+++ source/qcommon/steam.c
@@ -0,0 +1,128 @@
+/*
+Copyright (C) 2014 Victor Luchits
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+#include "../qcommon/qcommon.h"
+#include "../steamlib/steamlib_public.h"
+
+static steamlib_export_t *steamlib_export;
+static void *steamlib_libhandle = NULL;
+static qboolean steamlib_initialized = qfalse;
+
+void Steam_LoadLibrary( void );
+void Steam_UnloadLibrary( void );
+
+typedef void ( *com_error_t )( int code, const char *format, ... );
+
+/*
+* Steam_LoadLibrary
+*/
+void Steam_LoadLibrary( void )
+{
+	static steamlib_import_t import;
+	dllfunc_t funcs[2];
+	void *( *GetSteamLibAPI )(void *);
+
+	assert( !steamlib_libhandle );
+
+	import.Com_Error = (com_error_t)Com_Error;
+	import.Com_Printf = Com_Printf;
+	import.Com_DPrintf = Com_DPrintf;
+
+	// load dynamic library
+	Com_Printf( "Loading Steam module... " );
+	funcs[0].name = "GetSteamLibAPI";
+	funcs[0].funcPointer = (void **) &GetSteamLibAPI;
+	funcs[1].name = NULL;
+	steamlib_libhandle = Com_LoadLibrary( LIB_DIRECTORY "/steamlib_" ARCH LIB_SUFFIX, funcs );
+
+	if( steamlib_libhandle )
+	{
+		// load succeeded
+		int api_version;
+
+		steamlib_export = GetSteamLibAPI( &import );
+		api_version = steamlib_export->API();
+
+		if( api_version != STEAMLIB_API_VERSION )
+		{
+			// wrong version
+			Com_Printf( "Wrong version: %i, not %i.\n", api_version, STEAMLIB_API_VERSION );
+			Steam_UnloadLibrary();
+		}
+	}
+	else
+	{
+		Com_Printf( "Not found.\n" );
+	}
+}
+
+/*
+* Steam_UnloadLibrary
+*/
+void Steam_UnloadLibrary( void )
+{
+	assert( steamlib_libhandle );
+
+	if( steamlib_libhandle )
+	{
+		steamlib_export->Shutdown();
+	}
+
+	Com_UnloadLibrary( &steamlib_libhandle );
+	assert( !steamlib_libhandle );
+
+	steamlib_export = NULL;
+	steamlib_initialized = qfalse;
+
+	Com_Printf( "Steam module unloaded.\n" );
+}
+
+/*
+* Steam_Init
+*/
+void Steam_Init( void )
+{
+	if( steamlib_export ) {
+		if( !steamlib_export->Init() ) {
+			Com_Printf( "Steam initialization failed.\n" );
+			return;
+		}
+		steamlib_initialized = qtrue;
+	}
+}
+
+/*
+* Steam_RunFrame
+*/
+void Steam_RunFrame( void )
+{
+	if( steamlib_initialized ) {
+		steamlib_export->RunFrame();
+	}
+}
+
+/*
+* Steam_Shutdown
+*/
+void Steam_Shutdown( void )
+{
+	if( steamlib_initialized ) {
+		steamlib_export->Shutdown();
+	}
+}
---
+++ source/steamlib/steamlib_syscalls.cpp
@@ -0,0 +1,60 @@
+/*
+qfusion
+Copyright (c) 2014, Victor Luchits, All rights reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3.0 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library.
+*/
+
+#include "steamlib_local.h"
+
+namespace WSWSTEAM {
+
+static steamlib_import_t si;
+
+/*
+* GetSteamImport
+*/
+steamlib_import_t *GetSteamImport( void )
+{
+	return &si;
+}
+
+}
+
+/*
+* GetSteamLibAPI
+* 
+* Returns a pointer to the structure with all entry points
+*/
+extern "C" STEAMDLL_EXPORT steamlib_export_t *GetSteamLibAPI( steamlib_import_t *import )
+{
+	static steamlib_export_t globals;
+
+	WSWSTEAM::si = *import;
+
+	globals.API = &WSWSTEAM::SteamLib_API;
+
+	globals.Init = &WSWSTEAM::SteamLib_Init;
+	globals.RunFrame = &WSWSTEAM::SteamLib_RunFrame;
+	globals.Shutdown = &WSWSTEAM::SteamLib_Shutdown;
+
+	return &globals;
+}
+
+#if defined ( HAVE_DLLMAIN ) && !defined ( FTLIB_HARD_LINKED )
+int _stdcall DLLMain( void *hinstDll, unsigned long dwReason, void *reserved )
+{
+	return 1;
+}
+#endif
