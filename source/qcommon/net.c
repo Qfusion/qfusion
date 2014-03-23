6e18970b10db48f23dcc0d36956a03fdd582ed7f
Victor Luchits <vluchits@gmail.com>
2014-03-23 22:55:48 +0400

    Compilation fixes by Andrey Berman

--- source/qcommon/net.c
+++ source/qcommon/net.c
@@ -27,6 +27,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #else
 #include <unistd.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <sys/ioctl.h>
