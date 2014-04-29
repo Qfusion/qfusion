62abcf54516857d9435dcc7b53b352222fa27b4c
SiPlus <hwguy.siplus@gmail.com>
2014-04-29 12:13:26 +0400

    Sys_Library_Close qboolean

--- source/win32/win_lib.c
+++ source/win32/win_lib.c
@@ -29,7 +29,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 qboolean Sys_Library_Close( void *lib )
 {
-	return FreeLibrary( (HINSTANCE)lib );
+	return ( qboolean )FreeLibrary( (HINSTANCE)lib );
 }
 
 /*
