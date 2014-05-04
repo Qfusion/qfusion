e34732d857a359ed83300d86fbc2da3e92697160
SiPlus <hwguy.siplus@gmail.com>
2014-05-04 13:41:15 +0400

    Move skinning to an external GLSL file

--- glsl/include/vtransform.glsl
+++ glsl/include/vtransform.glsl
@@ -1,7 +1,21 @@
+#ifdef NUM_BONE_INFLUENCES
+
+attribute vec4 a_BonesIndices, a_BonesWeights;
+
+uniform vec4 u_DualQuats[MAX_UNIFORM_BONES*2];
+
+#define DUAL_QUAT_TRANSFORM_TANGENT
+#include "dualquat_overload.glsl"
+
+#undef DUAL_QUAT_TRANSFORM_TANGENT
+#include "dualquat_overload.glsl"
+
+#endif // NUM_BONE_INFLUENCES
+
 void TransformVerts(inout vec4 Position, inout vec3 Normal, inout vec2 TexCoord)
 {
 #ifdef NUM_BONE_INFLUENCES
-	QF_VertexDualQuatsTransform(NUM_BONE_INFLUENCES, Position, Normal);
+	VertexDualQuatsTransform(Position, Normal);
 #endif
 
 #ifdef APPLY_DEFORMVERTS
@@ -16,7 +30,7 @@ void TransformVerts(inout vec4 Position, inout vec3 Normal, inout vec2 TexCoord)
 void TransformVerts(inout vec4 Position, inout vec3 Normal, inout vec3 Tangent, inout vec2 TexCoord)
 {
 #ifdef NUM_BONE_INFLUENCES
-	QF_VertexDualQuatsTransform(NUM_BONE_INFLUENCES, Position, Normal, Tangent);
+	VertexDualQuatsTransform(Position, Normal, Tangent);
 #endif
 
 #ifdef APPLY_DEFORMVERTS
