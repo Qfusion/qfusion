8829542811d39819a41ca51633687fe890990a70
SiPlus <hwguy.siplus@gmail.com>
2014-05-05 22:00:49 +0400

    Change depth texture format to unsigned for GLES
    TODO: Add depth24 check in this place.

--- source/ref_gl/r_image.c
+++ source/ref_gl/r_image.c
@@ -591,7 +591,7 @@ static void R_Upload32( qbyte **data, int width, int height, int flags,
 	{
 		comp = GL_DEPTH_COMPONENT;
 		format = GL_DEPTH_COMPONENT;
-		type = GL_INT;
+		type = GL_UNSIGNED_INT;
 	}
 	else if( flags & IT_LUMINANCE )
 	{
