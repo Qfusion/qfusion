7a55d6a46ab5bd1fa2ca9a0b764e176fff7a0cb6
stopiccot <alexey.petruchik@gmail.com>
2014-05-26 17:21:09 +0300

    fixing FindRocket.cmake

--- source/cmake/FindRocket.cmake
+++ source/cmake/FindRocket.cmake
@@ -1,25 +1,29 @@
-# - Find ogg
-# Find the native ogg includes and libraries
+# - Find Rocket
+# Find the native libRocket includes and libraries
 #
-#  ANGELSCRIPT_INCLUDE_DIR - where to find angelscript.h, etc.
-#  ANGELSCRIPT_LIBRARIES   - List of libraries when using angelscript.
-#  ANGELSCRIPT_FOUND       - True if angelscript found.
+#  LIBROCKET_INCLUDE_DIR - where to find Rocket/Core.h, etc.
+#  LIBROCKET_LIBRARIES   - List of libraries when using libRocket.
+#  LIBROCKET_FOUND       - True if libRocket found.
 
-if(ROCKET_INCLUDE_DIR)
+if(LIBROCKET_INCLUDE_DIR)
     # Already in cache, be silent
-    set(ROCKET_FIND_QUIETLY TRUE)
-endif(ROCKET_INCLUDE_DIR)
-find_path(ROCKET_INCLUDE_DIR Rocket/Core.h)
-# MSVC built angelscript may be named angelscript_static.
+    set(LIBROCKET_FIND_QUIETLY TRUE)
+endif(LIBROCKET_INCLUDE_DIR)
+find_path(LIBROCKET_INCLUDE_DIR Rocket/Core.h)
+# MSVC built libRocket may be named angelscript_static.
 # The provided project files name the library with the lib prefix.
-find_library(ROCKET_CORE_LIBRARY NAMES RocketCore)
-find_library(ROCKET_CONTROLS_LIBRARY NAMES RocketControls)
+find_library(LIBROCKET_CORE_LIBRARY NAMES RocketCore Rocket)
+if (NOT ${ROCKET_CORE_LIBRARY} MATCHES ".framework")
+    find_library(LIBROCKET_CONTROLS_LIBRARY NAMES RocketControls)
+else()
+    set(LIBROCKET_CONTROLS_LIBRARY ${LIBROCKET_CORE_LIBRARY})
+endif()
 # Handle the QUIETLY and REQUIRED arguments and set ANGELSCRIPT_FOUND
 # to TRUE if all listed variables are TRUE.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ROCKET DEFAULT_MSG ROCKET_INCLUDE_DIR ROCKET_CORE_LIBRARY ROCKET_CONTROLS_LIBRARY)
+find_package_handle_standard_args(ROCKET DEFAULT_MSG LIBROCKET_INCLUDE_DIR LIBROCKET_CORE_LIBRARY LIBROCKET_CONTROLS_LIBRARY)
 
-set(ROCKET_LIBRARIES ${ROCKET_CORE_LIBRARY} ${ROCKET_CONTROLS_LIBRARY})
+set(LIBROCKET_LIBRARIES ${LIBROCKET_CORE_LIBRARY} ${LIBROCKET_CONTROLS_LIBRARY})
 
-mark_as_advanced(ROCKET_INCLUDE_DIR)
-mark_as_advanced(ROCKET_CORE_LIBRARY ROCKET_CONTROLS_LIBRARY)
+mark_as_advanced(LIBROCKET_INCLUDE_DIR)
+mark_as_advanced(LIBROCKET_CORE_LIBRARY LIBROCKET_CONTROLS_LIBRARY)
