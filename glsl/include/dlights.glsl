ae6d565bf005bdb063872bbb5bef86f53e7f09f4
SiPlus <hwguy.siplus@gmail.com>
2014-06-02 20:03:15 +0400

    Use inverse radius multiplication in dynamic lighting

--- glsl/include/dlights.glsl
+++ glsl/include/dlights.glsl
@@ -3,11 +3,13 @@
 struct DynamicLight
 {
 	vec3 Position;
-	myhalf4 DiffuseAndRadius;
+	myhalf4 DiffuseAndInvRadius;
 };
 
 uniform DynamicLight u_DynamicLights[NUM_DLIGHTS];
+#if !defined(GL_ES) && (QF_GLSL_VERSION >= 330)
 uniform int u_NumDynamicLights;
+#endif
 
 #include "dlights_overload.glsl"
 
