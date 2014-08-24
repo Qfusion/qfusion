9f363a63564c6e9ceff25f0095468a756fcb04c8
SiPlus <hwguy.siplus@gmail.com>
2014-08-24 17:31:30 +0400

    Draw color blend quad only when it's visible

--- source/cgame/cg_screen.cpp
+++ source/cgame/cg_screen.cpp
@@ -1296,7 +1296,8 @@ static void CG_SCRDrawViewBlend( void )
 		return;
 
 	CG_CalcColorBlend( colorblend );
-	trap_R_DrawStretchPic( 0, 0, cgs.vidWidth, cgs.vidHeight, 0, 0, 1, 1, colorblend, cgs.shaderWhite );
+	if( colorblend[3] > 0.0f )
+		trap_R_DrawStretchPic( 0, 0, cgs.vidWidth, cgs.vidHeight, 0, 0, 1, 1, colorblend, cgs.shaderWhite );
 }
 
 
