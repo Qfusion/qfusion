695cb64d162f0964d00a971278e6a55b9b308622
Victor Luchits <vluchits@gmail.com>
2014-09-13 00:49:21 +0400

    Use per-pixel fog on outlines

--- glsl/defaultOutline.frag.glsl
+++ glsl/defaultOutline.frag.glsl
@@ -1,7 +1,10 @@
 #include "include/common.glsl"
 #include "include/uniforms.glsl"
+
 #ifdef APPLY_FOG
 #include "include/fog.glsl"
+
+qf_varying vec2 v_FogCoord;
 #endif
 
 uniform float u_OutlineCutOff;
@@ -12,5 +15,11 @@ void main(void)
 	if (u_OutlineCutOff > 0.0 && (gl_FragCoord.z / gl_FragCoord.w > u_OutlineCutOff))
 		discard;
 #endif
+
+#ifdef APPLY_FOG
+	myhalf fogDensity = FogDensity(v_FogCoord);
+	qf_FragColor = vec4(vec3(mix(qf_FrontColor.rgb, u_FogColor, fogDensity)), 1.0);
+#else
 	qf_FragColor = vec4(qf_FrontColor);
+#endif
 }
