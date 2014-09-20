072739b6f4d3d5566487d42eb1a3487534da0b34
SiPlus <hwguy.siplus@gmail.com>
2014-09-20 22:31:29 +0400

    adem: Minor tuning of LG particles

--- source/cgame/cg_events.cpp
+++ source/cgame/cg_events.cpp
@@ -98,8 +98,8 @@ static void _LaserImpact( trace_t *trace, vec3_t dir )
 		if( laserOwner->localEffects[LOCALEFFECT_LASERBEAM_SMOKE_TRAIL] + TRAILTIME < cg.time )
 		{
 			laserOwner->localEffects[LOCALEFFECT_LASERBEAM_SMOKE_TRAIL] = cg.time;
-			CG_ImpactSmokePuff( trace->endpos, trace->plane.normal, 2, 1.0f, 8, 12 );
-			CG_ImpactPuffParticles( trace->endpos, trace->plane.normal, 4, 1.25f, 1.0f, 0.8f, 0.2f, 0.9f, NULL);
+			CG_ImpactSmokePuff( trace->endpos, trace->plane.normal, 1, 1.0f, 8, 12 );
+			CG_ImpactPuffParticles( trace->endpos, trace->plane.normal, 2, 0.9f, 1.0f, 0.8f, 0.2f, 1.0f, NULL);
 		}
 #undef TRAILTIME
 	}
