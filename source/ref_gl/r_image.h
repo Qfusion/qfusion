5dd8446171ddf3e314c60574f5d43c6a7318da32
Victor Luchits <vluchits@gmail.com>
2014-10-19 13:03:11 +0400

    Revert "Separate R_BindTexture into indirect and direct"
    This reverts commit e5f775cb7fd8bf8fe690a055308c785d79d4c7d5.

--- source/ref_gl/r_image.h
+++ source/ref_gl/r_image.h
@@ -69,7 +69,6 @@ typedef struct image_s
 } image_t;
 
 void R_SelectTextureUnit( int tmu );
-void R_SetTexture( int tmu, const image_t *tex );
 void R_BindTexture( int tmu, const image_t *tex );
 
 void R_InitImages( void );
