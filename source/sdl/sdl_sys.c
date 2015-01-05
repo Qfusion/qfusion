842a677348a5736c819378f3abd42fe8e0e62908
stopiccot <alexey.petruchik@gmail.com>
2015-01-05 18:26:33 +0300

    Futher SDL2 refactoring

--- source/sdl/sdl_sys.c
+++ source/sdl/sdl_sys.c
@@ -1,2 +1,40 @@
 #include <SDL.h>
 #include "../client/client.h"
+
+void Sys_Sleep( unsigned int millis )
+{
+	SDL_Delay( millis );
+}
+
+void Sys_Error( const char *format, ... )
+{
+	va_list argptr;
+	char msg[1024];
+
+	CL_Shutdown();
+
+	va_start( argptr, format );
+	Q_vsnprintfz( msg, sizeof( msg ), format, argptr );
+	va_end( argptr );
+
+	SDL_ShowSimpleMessageBox( SDL_MESSAGEBOX_ERROR, "qfusion", msg, NULL );
+
+	Qcommon_Shutdown();
+
+	exit( 1 );
+}
+
+/*
+* Sys_Init
+*/
+void Sys_Init( void )
+{
+	Sys_InitTime();
+}
+
+/*
+ * Sys_InitDynvars
+ */
+void Sys_InitDynvars( void )
+{
+}
