7d195445be26175d59fa3b69ffb350664b2121e7
SiPlus <hwguy.siplus@gmail.com>
2015-01-07 15:13:07 +0300

    Refactor Windows time code

--- source/win32/win_time.c
+++ source/win32/win_time.c
@@ -1,17 +1,98 @@
 #include "../qcommon/qcommon.h"
 #include <windows.h>
 
-qboolean	hwtimer;
-dynvar_t	*hwtimer_var;
-quint64		hwtimer_freq;
-int			milli_offset = 0;
-qint64		micro_offset = 0;
+static qboolean hwtimer;
+static dynvar_t *hwtimer_var;
+static quint64 hwtimer_freq;
+static int milli_offset = 0;
+static qint64 micro_offset = 0;
 
-/*
-* Sys_Milliseconds
-*/
 // wsw: pb adapted High Res Performance Counter code from ezquake
 
+/*
+* Sys_GetHwTimer_f
+*/
+static dynvar_get_status_t Sys_GetHwTimer_f( void **val )
+{
+	static char hwtimerStr[2] = { '\0', '\0' };
+	hwtimerStr[0] = '0' + hwtimer;
+	assert( val );
+	*val = hwtimerStr;
+	return DYNVAR_GET_OK;
+}
+
+/*
+* Sys_SetHwTimer_f
+*/
+static dynvar_set_status_t Sys_SetHwTimer_f( void *val )
+{
+	assert( val );
+	switch( *( (char *) val ) )
+	{
+	case '0':
+		hwtimer = 0;
+		return DYNVAR_SET_OK;
+	case '1':
+		if( hwtimer_freq )
+		{
+			hwtimer = 1;
+			return DYNVAR_SET_OK;
+		}
+		else
+			return DYNVAR_SET_TRANSIENT;
+	default:
+		return DYNVAR_SET_INVALID;
+	}
+}
+
+/*
+* Sys_SynchronizeTimers_f
+*/
+static void Sys_SynchronizeTimers_f( void *val )
+{
+	static int hwtimer_old = -1;
+
+	const unsigned int millis = Sys_Milliseconds_TGT();
+	const qint64 micros = Sys_Microseconds_QPC();
+	const qint64 drift = micros - millis * 1000;
+
+	const int hwtimer_new = ( *(char *) val ) - '0';
+
+	if( hwtimer_new != hwtimer_old )
+	{
+		switch( hwtimer_new )
+		{
+		case 0:
+			// switched from micro to milli precision
+			milli_offset = max( milli_offset, drift / 1000 );
+			break;
+		case 1:
+			// switched from milli to micro precision
+			micro_offset = max( micro_offset, -drift );
+			break;
+		default:
+			assert( 0 );
+		}
+		hwtimer_old = hwtimer_new;
+	}
+}
+
+/*
+* Sys_InitTimeDynvar
+*/
+void Sys_InitTimeDynvar( void )
+{
+	QueryPerformanceFrequency( (LARGE_INTEGER *) &hwtimer_freq );
+
+	hwtimer_var = Dynvar_Create( "sys_hwtimer", 1, Sys_GetHwTimer_f, Sys_SetHwTimer_f );
+	assert( hwtimer_var );
+	Dynvar_AddListener( hwtimer_var, Sys_SynchronizeTimers_f );
+	Dynvar_SetValue( hwtimer_var, "0" );
+}
+
+/*
+* Sys_InitTime
+*/
 void Sys_InitTime( void )
 {
 	char *hwtimerStr;
@@ -30,7 +111,11 @@ void Sys_InitTime( void )
 		Com_Printf( "Using timeGetTime\n" );
 }
 
-unsigned int Sys_Milliseconds_TGT( void )
+/*
+* Sys_Milliseconds
+*/
+
+static unsigned int Sys_Milliseconds_TGT( void )
 {
 	static unsigned int base;
 	static qboolean	initialized = qfalse;
@@ -49,7 +134,7 @@ unsigned int Sys_Milliseconds_TGT( void )
 	return now - base;
 }
 
-quint64 Sys_Microseconds_QPC( void )
+static quint64 Sys_Microseconds_QPC( void )
 {
 	static qboolean first = qtrue;
 	static qint64 p_start;
