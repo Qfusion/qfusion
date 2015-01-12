aae4d4c14579aa2195f8dd8afc765fb035f04773
stopiccot <alexey.petruchik@gmail.com>
2015-01-12 22:34:21 +0300

    SDL2 based lib loading implementation

---
+++ source/sdl/sdl_lib.c
@@ -0,0 +1,55 @@
+#include "../qcommon/qcommon.h"
+#include "../qcommon/sys_library.h"
+
+#include <SDL.h>
+
+/*
+* Sys_Library_Close
+*/
+qboolean Sys_Library_Close( void *lib )
+{
+	SDL_UnloadObject( lib );
+	return qtrue;
+}
+
+/*
+* Sys_Library_GetFullName
+*/
+const char *Sys_Library_GetFullName( const char *name )
+{
+	return FS_AbsoluteNameForBaseFile( name );
+}
+
+/*
+* Sys_Library_GetGameLibPath
+*/
+const char *Sys_Library_GetGameLibPath( const char *name, qint64 time, int randomizer )
+{
+	static char tempname[1024 * 10];
+	Q_snprintfz( tempname, sizeof( tempname ), "%s/%s/tempmodules_%lld_%d/%s", FS_WriteDirectory(), FS_GameDirectory(), time, randomizer, name );
+	return tempname;
+}
+
+/*
+* Sys_Library_Open
+*/
+void *Sys_Library_Open( const char *name )
+{
+	return SDL_LoadObject( name );
+}
+
+/*
+* Sys_Library_ProcAddress
+*/
+void *Sys_Library_ProcAddress( void *lib, const char *apifuncname )
+{
+	return SDL_LoadFunction( lib, apifuncname );
+}
+
+/*
+* Sys_Library_ErrorString
+*/
+const char *Sys_Library_ErrorString( void )
+{
+	return (char *)SDL_GetError();
+}
