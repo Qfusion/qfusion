99f345529c8dbe66210890b5eefb9db12c640caf
Victor Luchits <vluchits@gmail.com>
2015-01-24 22:00:46 +0300

    Parse .gtd files for gamtypes titles and descriptions

--- source/ui/datasources/ui_gametypes_datasource.cpp
+++ source/ui/datasources/ui_gametypes_datasource.cpp
@@ -3,48 +3,61 @@
 #include "kernel/ui_utils.h"
 #include "datasources/ui_gametypes_datasource.h"
 
-namespace
-{
-	struct cmp_gametypes_by_id
-	{
-		typedef std::pair<std::string, std::string> first_argument_type ;
-		typedef std::string second_argument_type;
-		typedef bool result_type;
-
-		result_type operator()(first_argument_type lhs, second_argument_type rhs) const
-		{
-			return lhs.first == rhs;
-		}
-	};
-}
 namespace WSWUI
 {
 	GameTypesDataSource::GameTypesDataSource():Rocket::Controls::DataSource("gametypes_source")
 	{
-		// Gametypes will appear in the list in the same order they appear in the
-		// `gameTypes' vector. We want the "stock" gametypes to appear BEFORE any
-		// other gametypes, so we'll prepopulate `gameTypes' with them
-		gameTypes.push_back(std::make_pair("dm", "Deathmatch"));
-		gameTypes.push_back(std::make_pair("ffa", "Free for All"));
-		gameTypes.push_back(std::make_pair("duel", "Duel"));
-		gameTypes.push_back(std::make_pair("tdm", "Team Deathmatch"));
-		gameTypes.push_back(std::make_pair("ctf", "Capture the Flag"));
-		gameTypes.push_back(std::make_pair("race", "Race"));
-		gameTypes.push_back(std::make_pair("ca", "Clan Arena"));
-		gameTypes.push_back(std::make_pair("bomb", "Bomb and Defuse"));
-		gameTypes.push_back(std::make_pair("ctftactics", "CTF: Tactics"));
-		gameTypes.push_back(std::make_pair("da", "Duel Arena"));
-		gameTypes.push_back(std::make_pair("headhunt", "Headhunt Deathmatch"));
-		gameTypes.push_back(std::make_pair("tdo", "Team Domination"));
-
 		std::vector<std::string> listedGameTypes;
 		getFileList(listedGameTypes, "progs/gametypes", ".gt");
 
 		for(std::vector<std::string>::const_iterator it = listedGameTypes.begin();
 		    it != listedGameTypes.end(); ++it)
 		{
-			if(std::find_if(gameTypes.begin(), gameTypes.end(), std::bind2nd(::cmp_gametypes_by_id(), *it)) == gameTypes.end())
-				gameTypes.push_back(std::make_pair(*it, *it));
+			if(std::find_if(gameTypes.begin(), gameTypes.end(), std::bind2nd(cmp_gametypes_by_id(), *it)) == gameTypes.end())
+			{
+				gametype gt(*it);
+
+				std::string filepath = std::string("progs/gametypes") + "/" + gt.name + ".gtd";
+				int filenum, filelen;
+
+				filelen = trap::FS_FOpenFile( filepath.c_str(), &filenum, FS_READ );
+				if( filenum ) {
+					if( filelen > 0 ){
+						char *buffer = new char[filelen+1], *end = buffer + filelen;
+						trap::FS_Read( buffer, filelen, filenum );
+						buffer[filelen] = '\0';
+
+						// parse title and description
+						char *ptr = buffer;
+
+						// parse single line of title
+						while( ptr ) {
+							const char *token = COM_ParseExt( &ptr, qtrue );
+							if( *token ) {
+								gt.title = "";
+							}
+							while( *token ) {
+								gt.title += (gt.title.empty() ? "" : " ");
+								gt.title += token;
+								token = COM_ParseExt( &ptr, false );
+							}
+							break;
+						}
+
+						// the rest is description
+						while( ptr && ptr < end && (*ptr == '\n' || *ptr == '\r' || *ptr == ' ')) ptr++;
+						if( ptr && ptr != end ) {
+							gt.description = ptr;
+						}
+
+						delete[] buffer;
+					}
+
+					trap::FS_FCloseFile( filenum );
+				}
+
+				gameTypes.push_back(gt);
+			}
 		}
 	}
 
@@ -58,8 +71,9 @@ namespace WSWUI
 		for(Rocket::Core::StringList::const_iterator it = cols.begin();
 			 it != cols.end();
 			 ++it){
-			if(*it == "val") row.push_back(gameTypes[row_index].first.c_str());
-			else if(*it ==  "name") row.push_back(gameTypes[row_index].second.c_str());
+			if(*it == "name") row.push_back(gameTypes[row_index].name.c_str());
+			else if(*it == "title") row.push_back(gameTypes[row_index].title.c_str());
+			else if(*it == "description") row.push_back(gameTypes[row_index].description.c_str());
 			else row.push_back("");
 		}
 	}
