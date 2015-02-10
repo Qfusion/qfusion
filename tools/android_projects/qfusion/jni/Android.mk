8a6b7e8c8a63d6024cfa1c106eee7c955041072d
SiPlus <hwguy.siplus@gmail.com>
2015-02-10 07:33:22 +0300

    Android build scripts: FreeType

---
+++ tools/android_projects/qfusion/jni/Android.mk
@@ -0,0 +1,10 @@
+QFUSION_PATH := $(call my-dir)
+
+include $(QFUSION_PATH)/angelscript.mk
+include $(QFUSION_PATH)/freetype.mk
+include $(QFUSION_PATH)/jpeg.mk
+include $(QFUSION_PATH)/png.mk
+
+include $(QFUSION_PATH)/angelwrap.mk
+include $(QFUSION_PATH)/ftlib.mk
+include $(QFUSION_PATH)/ref_gl.mk
