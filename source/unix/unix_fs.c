6e13bedbe1a5b39c97c3b5befd0690f2a8b618dd
SiPlus <hwguy.siplus@gmail.com>
2015-02-10 18:21:16 +0300

    Second type correction

--- source/unix/unix_fs.c
+++ source/unix/unix_fs.c
@@ -234,7 +234,7 @@ const char *Sys_FS_GetHomeDirectory( void )
 const char *Sys_FS_GetCacheDirectory( void )
 {
 #ifdef __ANDROID__
-	char cache[PATH_MAX] = { '\0' };
+	static char cache[PATH_MAX] = { '\0' };
 	if( !cache[0] )
 		Q_snprintfz( cache, sizeof( cache ), "/data/data/%s/cache", sys_android_packageName );
 	return cache;
