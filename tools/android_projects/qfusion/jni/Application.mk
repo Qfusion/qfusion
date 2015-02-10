f2085356392388ff9a3e794bf7e585dbbbeee856
SiPlus <hwguy.siplus@gmail.com>
2015-02-10 19:05:08 +0300

    Add x86 to APP_ABI

--- tools/android_projects/qfusion/jni/Application.mk
+++ tools/android_projects/qfusion/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := armeabi-v7a
+APP_ABI := armeabi-v7a x86
 APP_CFLAGS := -ffast-math -fno-strict-aliasing -funroll-loops -Werror=return-type
 APP_CPPFLAGS := -fexceptions -frtti
 APP_OPTIM := release
