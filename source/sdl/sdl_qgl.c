9fd02cabe586ad1cf6d007a6b3968a6096e39e60
Vic <vic@picmip.org>
2015-02-17 12:56:17 +0100

    Compilation warning fix

--- source/sdl/sdl_qgl.c
+++ source/sdl/sdl_qgl.c
@@ -143,12 +143,12 @@ qgl_initerr_t QGL_Init( const char *dllname )
 	}
 
 #define QGL_FUNC( type, name, params )                                   \
-	( q##name ) = (void *)qglGetProcAddress( #name );                    \
+	( q##name ) = (void *)qglGetProcAddress( (const GLubyte *)#name );   \
 	if( !( q##name ) ) {                                                 \
 		Com_Printf( "QGL_Init: Failed to get address for %s\n", #name ); \
 		return qgl_initerr_invalid_driver;                               \
 	}
-#define QGL_FUNC_OPT( type, name, params ) ( q##name ) = (void *)qglGetProcAddress( #name );
+#define QGL_FUNC_OPT( type, name, params ) ( q##name ) = (void *)qglGetProcAddress( (const GLubyte *)#name );
 #define QGL_EXT( type, name, params ) ( q##name ) = NULL;
 #define QGL_WGL( type, name, params )
 #define QGL_WGL_EXT( type, name, params )
