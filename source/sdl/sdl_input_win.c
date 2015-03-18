7a4dec65e0f67d21ee0a75d86af41b63d6253456
stopiccot <alexey.petruchik@gmail.com>
2015-03-18 23:54:23 +0300

    Show input language in SDL2 windows build

--- source/sdl/sdl_input_win.c
+++ source/sdl/sdl_input_win.c
@@ -1,10 +1,20 @@
 #include "../client/client.h"
+#include <windows.h>
 
 /**
  * IN_GetInputLanguage
  */
 void IN_GetInputLanguage( char *dest, size_t size )
 {
-	if( size )
-		dest[0] = '\0';
+	char lang[16];
+
+	lang[0] = '\0';
+
+	GetLocaleInfo(
+		MAKELCID( LOWORD( GetKeyboardLayout( 0 ) ), SORT_DEFAULT ),
+		LOCALE_SISO639LANGNAME,
+		lang, sizeof( lang ) );
+
+	Q_strupr( lang );
+	Q_strncpyz( dest, lang, size );
 }
