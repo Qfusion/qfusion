57c6ec5f7e92e351e76aff8b794ad06f942908a1
Vic <vic@picmip.org>
2015-03-19 11:17:07 +0100

    Include sys/time.h

--- source/qcommon/net.c
+++ source/qcommon/net.c
@@ -32,6 +32,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <arpa/inet.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #endif
 
 #define	MAX_LOOPBACK	4
