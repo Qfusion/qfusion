93345ecdc8ed7363ea5da7d69b9c4a3675dcb98c
SiPlus <hwguy.siplus@gmail.com>
2015-05-15 21:04:01 +0300

    Android code update

--- source/android/android_sys.h
+++ source/android/android_sys.h
@@ -22,32 +22,79 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define ANDROID_SYS_H
 
 #include "../qcommon/qcommon.h"
+#ifndef DEDICATED_ONLY
 #include "../client/client.h"
+#endif
 #include <android_native_app_glue.h>
 
+/**
+ * Custom looper data IDs.
+ */
+enum
+{
+	LOOPER_ID_USER_CLIENT = LOOPER_ID_USER
+};
+
+/**
+ * A pointer to the Native App Glue app.
+ */
 extern struct android_app *sys_android_app;
 
+/**
+ * A global reference to the class of the engine activity.
+ */
 extern jclass sys_android_activityClass;
 
+/**
+ * Commonly used JNI methods.
+ */
+extern jmethodID sys_android_getSystemService;
+
+/**
+ * The package name of the game.
+ */
 extern char sys_android_packageName[];
 
+/**
+ * Returns the JNI environment for the current thread.
+ * Attaches the Java VM to the current thread if needed.
+ * The VM is detached when the thread is finished.
+ *
+ * @return the JNI environment
+ */
 JNIEnv *Sys_Android_GetJNIEnv( void );
 
+/**
+ * Returns the time since the boot, in the SystemClock.uptimeMillis time base.
+ *
+ * @return milliseconds since the boot
+ */
 uint64_t Sys_Android_Microseconds( void );
 
-enum
-{
-	LOOPER_ID_QFUSION = LOOPER_ID_USER
-};
-
-enum
-{
-	SYS_ANDROID_EVENT_INPUT = 0x80,
-	SYS_ANDROID_EVENT_INPUT_CHAR = SYS_ANDROID_EVENT_INPUT
-};
-
 #ifndef DEDICATED_ONLY
-void IN_Android_OnQfusionEvent( uint8_t event, int readfd );
+
+/**
+ * Handles Native App Glue commands on the client side.
+ *
+ * @param app the Native App Glue app
+ * @param cmd the command
+ */
+void CL_Sys_Android_OnAppCmd( struct android_app *app, int32_t cmd );
+
+/**
+ * Sends a character from the event looper to the engine.
+ *
+ * @param charkey the character
+ */
+void IN_Android_CharEvent( int charkey );
+
+/**
+ * Attaches the refresh module to an Android window.
+ *
+ * @param window the Android native window
+ */
+void VID_Android_SetWindow( ANativeWindow *window );
+
 #endif
 
 #endif // ANDROID_SYS_H
