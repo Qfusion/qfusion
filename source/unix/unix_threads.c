339e8da2c1a6733f40bb46d78f0792993ab819b9
Victor Luchits <vluchits@gmail.com>
2015-05-31 11:52:38 +0300

    Forgot pthread_mutexattr_init last time

--- source/unix/unix_threads.c
+++ source/unix/unix_threads.c
@@ -43,10 +43,12 @@ int Sys_Mutex_Create( qmutex_t **pmutex )
 {
 	int res;
 	qmutex_t *mutex;
-	pthread_mutex_t m;
 	pthread_mutexattr_t mta;
+	pthread_mutex_t m;
 
+	pthread_mutexattr_init( &mta );
 	pthread_mutexattr_settype( &mta, PTHREAD_MUTEX_RECURSIVE );
+
 	res = pthread_mutex_init( &m, &mta );
 	if( res != 0 ) {
 		return res;
