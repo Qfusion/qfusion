63557661909d8814f327a140414cd738ac698dd2
Vic <vluchits@gmail.com>
2015-06-13 18:11:15 +0300

    Pass UTF8 application title to ref module

--- source/client/cl_vid.c
+++ source/client/cl_vid.c
@@ -128,7 +128,7 @@ static rserr_t VID_Sys_Init_( void *parentWindow, bool verbose )
 
 	xpm_icon = XPM_ParseIcon( sizeof( app256x256_xpm ) / sizeof( app256x256_xpm[0] ), app256x256_xpm );
 
-	res = VID_Sys_Init( APPLICATION, APP_SCREENSHOTS_PREFIX, APP_STARTUP_COLOR, xpm_icon,
+	res = VID_Sys_Init( APPLICATION_UTF8, APP_SCREENSHOTS_PREFIX, APP_STARTUP_COLOR, xpm_icon,
 		parentWindow, verbose );
 
 	free( xpm_icon );
