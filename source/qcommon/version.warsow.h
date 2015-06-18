28191e724de485708d01a4e13d2bbe77ef8bfb2d
Victor Luchits <vluchits@gmail.com>
2015-06-18 23:40:17 +0300

    Update version.warsow.h

--- source/qcommon/version.warsow.h
+++ source/qcommon/version.warsow.h
@@ -45,7 +45,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 #ifdef PUBLIC_BUILD
-#define APP_PROTOCOL_VERSION			20
+#define APP_PROTOCOL_VERSION			21
 #else
 #define APP_PROTOCOL_VERSION			6096	// we're using revision number as protocol version for internal builds
 #endif
@@ -75,7 +75,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define STR_TOSTR( x )					STR_HELPER( x )
 
 #define APP_PROTOCOL_VERSION_STR		STR_TOSTR( APP_PROTOCOL_VERSION )
-#define APP_DEMO_EXTENSION_STR			".wdz" APP_PROTOCOL_VERSION_STR
+#define APP_DEMO_PROTOCOL_VERSION_STR	STR_TOSTR( APP_DEMO_PROTOCOL_VERSION )
+#define APP_DEMO_EXTENSION_STR			".wdz" APP_DEMO_PROTOCOL_VERSION_STR
 
 #define APP_URI_SCHEME					APPLICATION "://"
 #define APP_URI_PROTO_SCHEME			APPLICATION STR_TOSTR( APP_PROTOCOL_VERSION ) "://"
