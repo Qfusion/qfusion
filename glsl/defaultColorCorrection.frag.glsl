570cbf2d7a94de062cfcd9d32d2496a6b3ffb458
SiPlus <hwguy.siplus@gmail.com>
2015-07-08 10:54:34 +0300

    Fix blue color in the color correction shader

--- glsl/defaultColorCorrection.frag.glsl
+++ glsl/defaultColorCorrection.frag.glsl
@@ -8,8 +8,9 @@ uniform sampler2D u_ColorLUT;
 
 void main(void)
 {
-	vec3 coords = qf_texture(u_BaseTexture, v_TexCoord).rgb * vec3(0.9375) + vec3(0.03125);
-	coords *= vec3(1.0, 0.0625, 16.0);
+	vec3 coords = qf_texture(u_BaseTexture, v_TexCoord).rgb;
+	coords.rg = coords.rg * vec2(0.9375) + vec2(0.03125);
+	coords *= vec3(1.0, 0.0625, 15.0);
 	float blueMix = fract(coords.b);
 	vec2 blueOffset = vec2(0.0, floor(coords.b) * 0.0625);
 	vec3 color1 = qf_texture(u_ColorLUT, coords.rg + blueOffset).rgb;
