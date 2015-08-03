221e52349fb54e1f1fd4d54aa868f0bdfa6aeea1
SiPlus <hwguy.siplus@gmail.com>
2015-08-03 18:28:57 +0300

    Display damage indicator only when got 5+ dmg from falling
    5dmg is received from wca1 jump pad miss

--- source/cgame/cg_events.cpp
+++ source/cgame/cg_events.cpp
@@ -905,7 +905,7 @@ void CG_Event_Fall( entity_state_t *state, int parm )
 
 		CG_StartFallKickEffect( ( parm + 5 ) * 10 );
 
-		if( parm > 0 )
+		if( parm >= 5 )
 			CG_DamageIndicatorAdd( parm, tv( 0, 0, 1 ) );
 	}
 
