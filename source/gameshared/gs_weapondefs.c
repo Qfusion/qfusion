736bbb1bf22a736d8b27c7f1afb1c6d2fd343ac1
Andrei Stepanov <adem4ik@gmail.com>
2015-10-02 00:15:33 +0300

    Lower reload frametime

--- source/gameshared/gs_weapondefs.c
+++ source/gameshared/gs_weapondefs.c
@@ -119,7 +119,7 @@ gs_weapon_definition_t gs_weaponDefs[] =
 			//timings (in msecs)
 			WEAPONUP_FRAMETIME,				// weapon up frametime
 			WEAPONDOWN_FRAMETIME,           // weapon down frametime
-			400,							// reload frametime
+			500,							// reload frametime
 			0,								// cooldown frametime
 			5000,							// projectile timeout
 			false,							// smooth refire
@@ -153,7 +153,7 @@ gs_weapon_definition_t gs_weaponDefs[] =
 			//timings (in msecs)
 			WEAPONUP_FRAMETIME,				// weapon up frametime
 			WEAPONDOWN_FRAMETIME,			// weapon down frametime
-			400,							// reload frametime
+			500,							// reload frametime
 			0,								// cooldown frametime
 			64,								// projectile timeout  / projectile range for instant weapons
 			false,							// smooth refire
