f3109a3c8c3f46b2ce4b0c888c53a2181395f1b3
Victor Luchits <vluchits@gmail.com>
2015-10-09 22:05:48 +0300

    Use a separate sound for player respawns

--- source/gameshared/gs_qrespath.h
+++ source/gameshared/gs_qrespath.h
@@ -266,6 +266,7 @@ extern "C" {
 //#define S_LAND					"sounds/misc/land"
 #define S_HIT_WATER		"sounds/misc/hit_water"
 
+#define S_PLAYER_RESPAWN "sounds/world/player_respawn"
 #define S_TELEPORT		"sounds/world/tele_in"
 #define S_JUMPPAD		"sounds/world/jumppad"
 #define S_LAUNCHPAD		"sounds/world/launchpad"
