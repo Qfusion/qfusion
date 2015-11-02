112b82738a96a8a44f6971344be106e29d2b806b
Victor Luchits <vluchits@gmail.com>
2015-11-02 21:27:27 +0300

    1150 ups rockets

--- source/gameshared/gs_weapondefs.c
+++ source/gameshared/gs_weapondefs.c
@@ -432,7 +432,7 @@ gs_weapon_definition_t gs_weaponDefs[] =
 			35,                             // splash minimum knockback
 
 			//projectile def
-			1100,							// speed
+			1150,							// speed
 			0,								// spread
 			0,                              // v_spread
 
@@ -467,7 +467,7 @@ gs_weapon_definition_t gs_weaponDefs[] =
 			35,                             // splash minimum knockback
 
 			//projectile def
-			1200,							// speed
+			1150,							// speed
 			0,								// spread
 			0,                              // v_spread
 
