4b6ec6afc1fb54782c002fce0c14507a1226dc90
SiPlus <hwguy.siplus@gmail.com>
2016-01-18 22:03:28 +0300

    Fix cubemap unbinding

--- source/ref_gl/r_backend.c
+++ source/ref_gl/r_backend.c
@@ -79,7 +79,8 @@ void RB_BeginRegistration( void )
 	for( i = MAX_TEXTURE_UNITS - 1; i >= 0; i-- ) {
 		RB_SelectTextureUnit( i );
 
-		qglBindTexture( GL_TEXTURE_CUBE_MAP_POSITIVE_X_ARB, 0 );
+		if( glConfig.ext.texture_cube_map )
+			qglBindTexture( GL_TEXTURE_CUBE_MAP_ARB, 0 );
 		if( glConfig.ext.texture_array )
 			qglBindTexture( GL_TEXTURE_2D_ARRAY_EXT, 0 );
 		if( glConfig.ext.texture3D )
