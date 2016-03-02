49b1669f5c1cf06ef665025339ad37e97503175a
dnk777 <dnk777@users.noreply.github.com>
2016-03-02 17:55:28 +0300

    Split bot.h to many headers, move Ai to ai_local.h

--- source/game/ai/AStar.cpp
+++ source/game/ai/AStar.cpp
@@ -19,7 +19,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#include "../g_local.h"
 #include "ai_local.h"
 
 //==========================================
