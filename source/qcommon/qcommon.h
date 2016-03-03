6aa2883f64a03d6cbdad281ff1da786bfd620d94
Victor Luchits <vluchits@gmail.com>
2016-03-03 12:07:51 +0300

    Reopen console log on SIGUSR1

--- source/qcommon/qcommon.h
+++ source/qcommon/qcommon.h
@@ -838,6 +838,7 @@ MISC
 void	    Com_BeginRedirect( int target, char *buffer, int buffersize, 
 				void ( *flush )(int, const char*, const void*), const void *extra );
 void	    Com_EndRedirect( void );
+void 	    Com_ReopenConsoleLog( void );
 void	    Com_Printf( const char *format, ... );
 void	    Com_DPrintf( const char *format, ... );
 void	    Com_Error( com_error_code_t code, const char *format, ... );
