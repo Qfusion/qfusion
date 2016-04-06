f7e7db8da7ce84c5d803b21307b526e1efc43b07
dnk777 <dnk777@users.noreply.github.com>
2016-04-07 01:54:21 +0300

    BOT_Respawn: Move all Ai/Bot calls to Bot::OnRespawn

--- source/game/ai/bot_spawn.cpp
+++ source/game/ai/bot_spawn.cpp
@@ -104,13 +104,10 @@ void BOT_Respawn( edict_t *self )
 		return;
 
 	self->pain = BOT_pain;
-
 	self->ai->botRef->OnRespawn();
 
 	VectorClear( self->r.client->ps.pmove.delta_angles );
 	self->r.client->level.last_activity = level.time;
-
-	self->ai->aiRef->ResetNavigation();
 }
 
 static float MakeRandomBotSkillByServerSkillLevel()
