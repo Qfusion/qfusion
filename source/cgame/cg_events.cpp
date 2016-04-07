814c68dd7c3d956aefe4cb5efbb83ec1261eff47
Gerco van Heerdt <gercovanheerdt@gmail.com>
2016-04-07 09:58:10 +0200

    Enable firing and "breathing" anims for cg_gunbob 0

--- source/cgame/cg_events.cpp
+++ source/cgame/cg_events.cpp
@@ -436,7 +436,7 @@ static void CG_FireWeaponEvent( int entNum, int weapon, int fireMode )
 	}
 
 	// add animation to the view weapon model
-	if( ISVIEWERENTITY( entNum ) && !cg.view.thirdperson && cg_gunbob->integer )
+	if( ISVIEWERENTITY( entNum ) && !cg.view.thirdperson )
 		CG_ViewWeapon_StartAnimationEvent( fireMode == FIRE_MODE_STRONG ? WEAPMODEL_ATTACK_STRONG : WEAPMODEL_ATTACK_WEAK );
 }
 
