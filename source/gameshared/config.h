91206e5b7274b2b13dc54e517aba9b211319af3c
Vic <vic@picmip.org>
2016-04-19 00:34:24 +0200

    Define PUBLIC_BUILD

--- source/gameshared/config.h
+++ source/gameshared/config.h
@@ -3,7 +3,7 @@
 /* define this when we compile for a public release                     */
 /* this will protect dangerous and untested pieces of code              */
 /************************************************************************/
-//#define PUBLIC_BUILD
+#define PUBLIC_BUILD
 //#define BROKEN_BUILD
 
 //==============================================
