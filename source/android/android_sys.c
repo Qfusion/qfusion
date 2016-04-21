3c62688abb6b9a0f5c04fdde34e214716e12d39f
3angel <hwguy.siplus@gmail.com>
2016-04-21 18:28:48 +0300

    Remove the mkdir call in android_sys

--- source/android/android_sys.c
+++ source/android/android_sys.c
@@ -24,7 +24,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <android/log.h>
 #include <android/window.h>
 #include <linux/limits.h>
-#include <sys/stat.h>
 
 unsigned int sys_frame_time;
 
@@ -331,10 +330,9 @@ static void Sys_Android_Init( void )
 	// Set working directory to external data path.
 	{
 		char externalDataPath[PATH_MAX];
-		Q_snprintfz( externalDataPath, sizeof( externalDataPath ), "%s/%d.%d",
+		Q_snprintfz( externalDataPath, sizeof( externalDataPath ), "%s/%d.%d/", // The trailing slash is here to make it a directory path, not a file path
 			app->activity->externalDataPath, APP_VERSION_MAJOR, APP_VERSION_MINOR );
 		FS_CreateAbsolutePath( externalDataPath );
-		mkdir( externalDataPath, 0777 );
 		if( chdir( externalDataPath ) )
 			Sys_Error( "Failed to change working directory" );
 	}
