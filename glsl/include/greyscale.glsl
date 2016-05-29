60439d8dea017e5335f123758b003f990e2f4cf6
Victor Luchits <vluchits@gmail.com>
2016-05-29 16:15:42 +0300

    Moved GLSL scripts to a separate repository

--- glsl/include/greyscale.glsl
+++
@@ -1,4 +0,0 @@
-myhalf3 Greyscale(myhalf3 color)
-{
-	return myhalf3(dot(color, myhalf3(0.299, 0.587, 0.114)));
-}
--- glsl/include/rgbdepth.glsl
+++
@@ -1,7 +0,0 @@
-#ifndef decodedepthmacro24
-#define decodedepthmacro24(d) dot((d).rgb, vec3(1.0, 1.0 / 255.0, 1.0 / 65025.0))
-#define encodedepthmacro24(d) fract(d * vec4(1.0, 255.0, 65025.0, 0.0))
-// Use green for near because it has one more bit of precision
-#define decodedepthmacro16(d) dot((d).rgb, vec3(1.0, 1.0 / 961.0, 1.0 / 31.0))
-#define encodedepthmacro16(d) fract(d * vec4(1.0, 961.0, 31.0, 0.0))
-#endif
