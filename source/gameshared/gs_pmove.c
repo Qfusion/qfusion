c93fe874545b550c4da1cc1cb2b02f7388960e98
Gerco van Heerdt <gercovanheerdt@gmail.com>
2016-07-04 22:57:03 +0200

    No skimming on the ground

--- source/gameshared/gs_pmove.c
+++ source/gameshared/gs_pmove.c
@@ -415,7 +415,7 @@ static int PM_SlideMove( void )
 
 	if( pm->numtouch )
 	{
-		if( pm->playerState->pmove.pm_time || ( pm->playerState->pmove.skim_time > 0 && old_velocity[2] >= pml.velocity[2] ) )
+		if( pm->playerState->pmove.pm_time || ( pm->groundentity == -1 && pm->playerState->pmove.skim_time > 0 && old_velocity[2] >= pml.velocity[2] ) )
 			VectorCopy( old_velocity, pml.velocity );
 		pm->playerState->pmove.skim_time -= pm->cmd.msec;
 	}
