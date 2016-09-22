42b8983dae2c2e3f2b12dea3b99624cda9b14062
Victor Luchits <vluchits@gmail.com>
2016-09-22 12:39:52 +0300

    This should fix MacOS build

--- source/game/g_as_local.h
+++ source/game/g_as_local.h
@@ -18,12 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
-#if defined ( __APPLE__ )
-#include "angelscript/angelscript.h"
-#else
 #include "angelscript.h"
-#endif
-
 #include "../gameshared/q_angeliface.h"
 
 #define G_AsMalloc								G_LevelMalloc
