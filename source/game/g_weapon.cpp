Qfusion/qfusion

commit 82ce50b5a437020c8dba83c4e78ee2fa3a098e6b

New networking code for delta-encoded entity and ucmd fields
The new protocol uses static tables with field offsets, with up to 256 entity fields.
The U_ and CM_ bitfield constants are now gone.
Most integer fields now use base128 or zigzag-base128 encoding of variable length.
Most of the basic integer MSG_Write/MSG_Read functions renamed to reflect the underlying bit size.

source/game/g_weapon.cpp
@@ -254,7 +254,6 @@ static edict_t *W_Fire_LinearProjectile( edict_t *self, vec3_t start, vec3_t ang
 	GS_SnapVelocity( projectile->velocity );
 
 	projectile->movetype = MOVETYPE_LINEARPROJECTILE;
-	projectile->s.linearMovement = true;
 
 	projectile->r.solid = SOLID_YES;
 	projectile->r.clipmask = ( !GS_RaceGametype() ) ? MASK_SHOT : MASK_SOLID;
@@ -262,7 +261,6 @@ static edict_t *W_Fire_LinearProjectile( edict_t *self, vec3_t start, vec3_t ang
 	projectile->r.svflags = SVF_PROJECTILE;
 
 	// enable me when drawing exception is added to cgame
-	projectile->r.svflags |= SVF_TRANSMITORIGIN2;
 	VectorClear( projectile->r.mins );
 	VectorClear( projectile->r.maxs );
 	projectile->s.modelindex = 0;
@@ -275,7 +273,6 @@ static edict_t *W_Fire_LinearProjectile( edict_t *self, vec3_t start, vec3_t ang
 	projectile->style = 0;
 	projectile->s.sound = 0;
 	projectile->timeStamp = level.time;
-	projectile->s.linearMovementTimeStamp = game.serverTime;
 	projectile->timeDelta = timeDelta;
 
 	projectile->projectileInfo.minDamage = min( minDamage, damage );
@@ -288,7 +285,10 @@ static edict_t *W_Fire_LinearProjectile( edict_t *self, vec3_t start, vec3_t ang
 	GClip_LinkEntity( projectile );
 
 	// update some data required for the transmission
+	projectile->s.linearMovement = true;
+	VectorCopy( projectile->s.origin, projectile->s.linearMovementBegin );
 	VectorCopy( projectile->velocity, projectile->s.linearMovementVelocity );
+	projectile->s.linearMovementTimeStamp = game.serverTime;
 	projectile->s.team = self->s.team;
 	projectile->s.modelindex2 = ( abs( timeDelta ) > 255 ) ? 255 : (unsigned int)abs( timeDelta );
 	return projectile;
@@ -390,7 +390,6 @@ void W_Fire_Blade( edict_t *self, int range, vec3_t start, vec3_t angles, float 
 		event = G_SpawnEvent( EV_BLADE_IMPACT, 0, end );
 		event->s.ownerNum = ENTNUM( self );
 		VectorScale( trace.plane.normal, 1024, event->s.origin2 );
-		event->r.svflags = SVF_TRANSMITORIGIN2;
 		return;
 	}
 
@@ -487,7 +486,6 @@ void W_Fire_Bullet( edict_t *self, vec3_t start, vec3_t angles, int seed, int ra
 	// send the event
 	event = G_SpawnEvent( EV_FIRE_BULLET, seed, start );
 	event->s.ownerNum = ENTNUM( self );
-	event->r.svflags = SVF_TRANSMITORIGIN2;
 	VectorScale( dir, 4096, event->s.origin2 ); // DirToByte is too inaccurate
 	event->s.weapon = WEAP_MACHINEGUN;
 	event->s.firemode = ( mod == MOD_MACHINEGUN_S ) ? FIRE_MODE_STRONG : FIRE_MODE_WEAK;
@@ -575,7 +573,6 @@ void W_Fire_Riotgun( edict_t *self, vec3_t start, vec3_t angles, int seed, int r
 	// send the event
 	event = G_SpawnEvent( EV_FIRE_RIOTGUN, seed, start );
 	event->s.ownerNum = ENTNUM( self );
-	event->r.svflags = SVF_TRANSMITORIGIN2;
 	VectorScale( dir, 4096, event->s.origin2 ); // DirToByte is too inaccurate
 	event->s.weapon = WEAP_RIOTGUN;
 	event->s.firemode = ( mod == MOD_RIOTGUN_S ) ? FIRE_MODE_STRONG : FIRE_MODE_WEAK;
@@ -1088,7 +1085,6 @@ void W_Fire_Electrobolt_Combined( edict_t *self, vec3_t start, vec3_t angles, fl
 
 	// send the weapon fire effect
 	event = G_SpawnEvent( EV_ELECTROTRAIL, ENTNUM( self ), start );
-	event->r.svflags = SVF_TRANSMITORIGIN2;
 	VectorScale( dir, 1024, event->s.origin2 );
 	event->s.firemode = fireMode;
 
@@ -1199,7 +1195,6 @@ void W_Fire_Electrobolt_FullInstant( edict_t *self, vec3_t start, vec3_t angles,
 
 	// send the weapon fire effect
 	event = G_SpawnEvent( EV_ELECTROTRAIL, ENTNUM( self ), start );
-	event->r.svflags = SVF_TRANSMITORIGIN2;
 	VectorScale( dir, 1024, event->s.origin2 );
 	event->s.firemode = FIRE_MODE_STRONG;
 
@@ -1317,7 +1312,6 @@ void W_Fire_Instagun( edict_t *self, vec3_t start, vec3_t angles, float damage, 
 
 	// send the weapon fire effect
 	event = G_SpawnEvent( EV_INSTATRAIL, ENTNUM( self ), start );
-	event->r.svflags = SVF_TRANSMITORIGIN2;
 	VectorScale( dir, 1024, event->s.origin2 );
 }
 
@@ -1414,7 +1408,6 @@ static edict_t *_FindOrSpawnLaser( edict_t *owner, int entType, bool *newLaser )
 		laser->s.ownerNum = ownerNum;
 		laser->movetype = MOVETYPE_NONE;
 		laser->r.solid = SOLID_NOT;
-		laser->r.svflags = SVF_TRANSMITORIGIN2;
 		laser->s.modelindex = 255; // needs to have some value so it isn't filtered by the server culling
 	}
 

source/cgame/cg_ents.cpp
@@ -182,7 +182,9 @@ static void CG_NewPacketEntityState( entity_state_t *state ) {
 		}
 
 		// some data changes will force no lerping
-		if( ( state->modelindex != cent->current.modelindex ) || state->teleported || state->linearMovement != cent->current.linearMovement ) {
+		if( state->modelindex != cent->current.modelindex
+			|| state->teleported 
+			|| state->linearMovement != cent->current.linearMovement ) {
 			cent->serverFrame = -99;
 		}
 
@@ -192,15 +194,6 @@ static void CG_NewPacketEntityState( entity_state_t *state ) {
 			cent->prev = *state;
 
 			memset( cent->localEffects, 0, sizeof( cent->localEffects ) );
-
-			// new entities may have old_origin
-			if( state->svflags & SVF_TRANSMITORIGIN2 &&
-				( state->type == ET_GENERIC || state->type == ET_GIB
-				  || state->type == ET_GRENADE || state->type == ET_SPRITE
-				  || state->type == ET_ITEM || state->type == ET_FLAG_BASE
-				  || state->type == ET_PARTICLES || state->type == ET_RADAR ) ) {
-				VectorCopy( state->old_origin, cent->prev.origin );
-			}
 
 			// Init the animation when new into PVS
 			if( cg.frame.valid && ( state->type == ET_PLAYER || state->type == ET_CORPSE ) ) {
@@ -234,7 +227,7 @@ static void CG_NewPacketEntityState( entity_state_t *state ) {
 		cent->serverFrame = cg.frame.serverFrame;
 
 		// set up velocities for this entity
-		if( cgs.extrapolationTime && ( state->svflags & SVF_TRANSMITORIGIN2 ) &&
+		if( cgs.extrapolationTime &&
 			( cent->current.type == ET_PLAYER || cent->current.type == ET_CORPSE ) ) {
 			VectorCopy( cent->current.origin2, cent->velocity );
 			VectorCopy( cent->prev.origin2, cent->prevVelocity );
@@ -710,7 +703,8 @@ void CG_LerpGenericEnt( centity_t *cent ) {
 		// do the animation properly
 		vec3_t delta, move;
 
-		VectorSubtract( cent->current.old_origin, cent->current.origin, move );
+		// FIXME: does this still work?
+		VectorSubtract( cent->current.origin2, cent->current.origin, move );
 		Matrix3_TransformVector( cent->ent.axis, move, delta );
 		VectorMA( cent->current.origin, cent->ent.backlerp, delta, cent->ent.origin );
 	} else if( ISVIEWERENTITY( cent->current.number ) || cg.view.POVent == cent->current.number ) {
@@ -2205,10 +2199,6 @@ void CG_UpdateEntities( void ) {
 				break;
 
 			default:
-				if( cent->type & ET_INVERSE ) {
-					CG_Printf( "CG_UpdateEntities: entity type with ET_INVERSE. Stripped type %i", cent->type & 127 );
-				}
-
 				CG_Error( "CG_UpdateEntities: unknown entity type %i", cent->type );
 				break;
 		}

source/cgame/cg_events.cpp
@@ -1129,8 +1129,8 @@ void CG_EntityEvent( entity_state_t *ent, int ev, int parm, bool predicted ) {
 
 		case EV_WEAPONACTIVATE:
 			CG_PModel_AddAnimation( ent->number, 0, TORSO_WEAPON_SWITCHIN, 0, EVENT_CHANNEL );
-			weapon = ( parm & ~EV_INVERSE );
-			fireMode = ( parm & EV_INVERSE ) ? FIRE_MODE_STRONG : FIRE_MODE_WEAK;
+			weapon = ( parm >> 1 ) & 0x3f;
+			fireMode = ( parm & 0x1 ) ? FIRE_MODE_STRONG : FIRE_MODE_WEAK;
 			if( predicted ) {
 				cg_entities[ent->number].current.weapon = weapon;
 				if( fireMode == FIRE_MODE_STRONG ) {
@@ -1153,8 +1153,8 @@ void CG_EntityEvent( entity_state_t *ent, int ev, int parm, bool predicted ) {
 
 		case EV_SMOOTHREFIREWEAPON: // the server never sends this event
 			if( predicted ) {
-				weapon = ( parm & ~EV_INVERSE );
-				fireMode = ( parm & EV_INVERSE ) ? FIRE_MODE_STRONG : FIRE_MODE_WEAK;
+				weapon = ( parm >> 1 ) & 0x3f;
+				fireMode = ( parm & 0x1 ) ? FIRE_MODE_STRONG : FIRE_MODE_WEAK;
 
 				cg_entities[ent->number].current.weapon = weapon;
 				if( fireMode == FIRE_MODE_STRONG ) {
@@ -1170,8 +1170,8 @@ void CG_EntityEvent( entity_state_t *ent, int ev, int parm, bool predicted ) {
 			break;
 
 		case EV_FIREWEAPON:
-			weapon = ( parm & ~EV_INVERSE );
-			fireMode = ( parm & EV_INVERSE ) ? FIRE_MODE_STRONG : FIRE_MODE_WEAK;
+			weapon = ( parm >> 1 ) & 0x3f;
+			fireMode = ( parm & 0x1 ) ? FIRE_MODE_STRONG : FIRE_MODE_WEAK;
 
 			if( predicted ) {
 				cg_entities[ent->number].current.weapon = weapon;
@@ -1357,7 +1357,7 @@ void CG_EntityEvent( entity_state_t *ent, int ev, int parm, bool predicted ) {
 								COLOR_R( ent->colorRGBA ) * ( 1.0 / 255.0 ),
 								COLOR_G( ent->colorRGBA ) * ( 1.0 / 255.0 ),
 								COLOR_B( ent->colorRGBA ) * ( 1.0 / 255.0 ),
-								ent->eventCount );
+								ent->counterNum );
 			break;
 
 		case EV_GESTURE:

source/client/cl_input.c
@@ -323,22 +323,22 @@ void CL_WriteUcmdsToMessage( msg_t *msg ) {
 	}
 
 	// begin a client move command
-	MSG_WriteByte( msg, clc_move );
+	MSG_WriteUint8( msg, clc_move );
 
 	// (acknowledge server frame snap)
 	// let the server know what the last frame we
 	// got was, so the next message can be delta compressed
 	if( cl.receivedSnapNum <= 0 ) {
-		MSG_WriteLong( msg, -1 );
+		MSG_WriteInt32( msg, -1 );
 	} else {
-		MSG_WriteLong( msg, cl.snapShots[cl.receivedSnapNum & UPDATE_MASK].serverFrame );
+		MSG_WriteInt32( msg, cl.snapShots[cl.receivedSnapNum & UPDATE_MASK].serverFrame );
 	}
 
 	// Write the actual ucmds
 
 	// write the id number of first ucmd to be sent, and the count
-	MSG_WriteLong( msg, ucmdHead );
-	MSG_WriteByte( msg, (uint8_t)( ucmdHead - ucmdFirst ) );
+	MSG_WriteInt32( msg, ucmdHead );
+	MSG_WriteUint8( msg, (uint8_t)( ucmdHead - ucmdFirst ) );
 
 	// write the ucmds
 	for( i = ucmdFirst; i < ucmdHead; i++ ) {

source/client/cl_main.c
@@ -139,9 +139,9 @@ void CL_UpdateClientCommandsToServer( msg_t *msg ) {
 			continue;
 		}
 
-		MSG_WriteByte( msg, clc_clientcommand );
+		MSG_WriteUint8( msg, clc_clientcommand );
 		if( !cls.reliable ) {
-			MSG_WriteLong( msg, i );
+			MSG_WriteInt32( msg, i );
 		}
 		MSG_WriteString( msg, cls.reliableCommands[i & ( MAX_RELIABLE_COMMANDS - 1 )] );
 	}
@@ -1051,7 +1051,7 @@ static void CL_ConnectionlessPacket( const socket_t *socket, const netadr_t *add
 	char *c;
 
 	MSG_BeginReading( msg );
-	MSG_ReadLong( msg ); // skip the -1
+	MSG_ReadInt32( msg ); // skip the -1
 
 	s = MSG_ReadStringLine( msg );
 
@@ -1255,8 +1255,8 @@ static bool CL_ProcessPacket( netchan_t *netchan, msg_t *msg ) {
 	}
 	// now if compressed, expand it
 	MSG_BeginReading( msg );
-	MSG_ReadLong( msg ); // sequence
-	MSG_ReadLong( msg ); // sequence_ack
+	MSG_ReadInt32( msg ); // sequence
+	MSG_ReadInt32( msg ); // sequence_ack
 	if( msg->compressed ) {
 		zerror = Netchan_DecompressMessage( msg );
 		if( zerror < 0 ) {
@@ -2350,8 +2350,8 @@ void CL_SendMessagesToServer( bool sendNow ) {
 		if( sendNow || cls.realtime > 100 + cls.lastPacketSentTime ) {
 			// write the command ack
 			if( !cls.reliable ) {
-				MSG_WriteByte( &message, clc_svcack );
-				MSG_WriteLong( &message, (unsigned int)cls.lastExecutedServerCommand );
+				MSG_WriteUint8( &message, clc_svcack );
+				MSG_WriteInt32( &message, (unsigned int)cls.lastExecutedServerCommand );
 			}
 			//write up the clc commands
 			CL_UpdateClientCommandsToServer( &message );
@@ -2362,8 +2362,8 @@ void CL_SendMessagesToServer( bool sendNow ) {
 	} else if( sendNow || CL_MaxPacketsReached() ) {
 		// write the command ack
 		if( !cls.reliable ) {
-			MSG_WriteByte( &message, clc_svcack );
-			MSG_WriteLong( &message, (unsigned int)cls.lastExecutedServerCommand );
+			MSG_WriteUint8( &message, clc_svcack );
+			MSG_WriteInt32( &message, (unsigned int)cls.lastExecutedServerCommand );
 		}
 		// send a userinfo update if needed
 		if( userinfo_modified ) {

source/client/cl_parse.c
@@ -726,8 +726,8 @@ static void CL_ParseDownload( msg_t *msg ) {
 
 	// read the data
 	svFilename = MSG_ReadString( msg );
-	offset = MSG_ReadLong( msg );
-	size = MSG_ReadLong( msg );
+	offset = MSG_ReadInt32( msg );
+	size = MSG_ReadInt32( msg );
 
 	if( cls.demo.playing ) {
 		// ignore download commands coming from demo files
@@ -816,14 +816,14 @@ static void CL_ParseServerData( msg_t *msg ) {
 	CL_SetClientState( CA_CONNECTED );
 
 	// parse protocol version number
-	i = MSG_ReadLong( msg );
+	i = MSG_ReadInt32( msg );
 
 	if( i != APP_PROTOCOL_VERSION && !( cls.demo.playing && i == APP_DEMO_PROTOCOL_VERSION ) ) {
 		Com_Error( ERR_DROP, "Server returned version %i, not %i", i, APP_PROTOCOL_VERSION );
 	}
 
-	cl.servercount = MSG_ReadLong( msg );
-	cl.snapFrameTime = (unsigned int)MSG_ReadShort( msg );
+	cl.servercount = MSG_ReadInt32( msg );
+	cl.snapFrameTime = (unsigned int)MSG_ReadInt16( msg );
 	cl.gamestart = true;
 
 	// set extrapolation time to half snapshot time
@@ -865,12 +865,12 @@ static void CL_ParseServerData( msg_t *msg ) {
 	}
 
 	// parse player entity number
-	cl.playernum = MSG_ReadShort( msg );
+	cl.playernum = MSG_ReadInt16( msg );
 
 	// get the full level name
 	Q_strncpyz( cl.servermessage, MSG_ReadString( msg ), sizeof( cl.servermessage ) );
 
-	sv_bitflags = MSG_ReadByte( msg );
+	sv_bitflags = MSG_ReadUint8( msg );
 
 	if( cls.demo.playing ) {
 		cls.reliable = ( sv_bitflags & SV_BITFLAGS_RELIABLE );
@@ -891,7 +891,7 @@ static void CL_ParseServerData( msg_t *msg ) {
 			// read base upstream url
 			cls.httpbaseurl = ZoneCopyString( MSG_ReadString( msg ) );
 		} else {
-			http_portnum = MSG_ReadShort( msg ) & 0xffff;
+			http_portnum = MSG_ReadInt16( msg ) & 0xffff;
 			cls.httpaddress = cls.serveraddress;
 			if( cls.httpaddress.type == NA_IP6 ) {
 				cls.httpaddress.address.ipv6.port = BigShort( http_portnum );
@@ -914,10 +914,10 @@ static void CL_ParseServerData( msg_t *msg ) {
 	Com_FreePureList( &cls.purelist );
 
 	// add new
-	numpure = MSG_ReadShort( msg );
+	numpure = MSG_ReadInt16( msg );
 	while( numpure > 0 ) {
 		const char *pakname = MSG_ReadString( msg );
-		const unsigned checksum = MSG_ReadLong( msg );
+		const unsigned checksum = MSG_ReadInt32( msg );
 
 		Com_AddPakToPureList( &cls.purelist, pakname, checksum, NULL );
 
@@ -1211,7 +1211,7 @@ void CL_ParseServerMessage( msg_t *msg ) {
 			break;
 		}
 
-		cmd = MSG_ReadByte( msg );
+		cmd = MSG_ReadUint8( msg );
 		if( cl_debug_serverCmd->integer & 4 ) {
 			if( cmd == -1 ) {
 				Com_Printf( "%3i:CMD %i %s\n", msg->readcount - 1, cmd, "EOF" );
@@ -1245,7 +1245,7 @@ void CL_ParseServerMessage( msg_t *msg ) {
 
 			case svc_servercmd:
 				if( !cls.reliable ) {
-					int cmdNum = MSG_ReadLong( msg );
+					int cmdNum = MSG_ReadInt32( msg );
 					if( cmdNum < 0 ) {
 						Com_Error( ERR_DROP, "CL_ParseServerMessage: Invalid cmdNum value received: %i\n",
 								   cmdNum );
@@ -1284,8 +1284,8 @@ void CL_ParseServerMessage( msg_t *msg ) {
 					Com_Error( ERR_DROP, "CL_ParseServerMessage: clack message for reliable client\n" );
 					return;
 				}
-				cls.reliableAcknowledge = (unsigned)MSG_ReadLong( msg );
-				cls.ucmdAcknowledged = (unsigned)MSG_ReadLong( msg );
+				cls.reliableAcknowledge = (unsigned)MSG_ReadInt32( msg );
+				cls.ucmdAcknowledged = (unsigned)MSG_ReadInt32( msg );
 				if( cl_debug_serverCmd->integer & 4 ) {
 					Com_Printf( "svc_clcack:reliable cmd ack:%i ucmdack:%i\n", cls.reliableAcknowledge, cls.ucmdAcknowledged );
 				}
@@ -1300,10 +1300,10 @@ void CL_ParseServerMessage( msg_t *msg ) {
 				{
 					size_t meta_data_maxsize;
 
-					MSG_ReadLong( msg );
-					MSG_ReadLong( msg );
-					cls.demo.meta_data_realsize = (size_t)MSG_ReadLong( msg );
-					meta_data_maxsize = (size_t)MSG_ReadLong( msg );
+					MSG_ReadInt32( msg );
+					MSG_ReadInt32( msg );
+					cls.demo.meta_data_realsize = (size_t)MSG_ReadInt32( msg );
+					meta_data_maxsize = (size_t)MSG_ReadInt32( msg );
 
 					// sanity check
 					if( cls.demo.meta_data_realsize > meta_data_maxsize ) {
@@ -1328,9 +1328,9 @@ void CL_ParseServerMessage( msg_t *msg ) {
 				if( 1 ) {
 					int ext, len;
 
-					ext = MSG_ReadByte( msg );  // extension id
-					MSG_ReadByte( msg );        // version number
-					len = MSG_ReadShort( msg ); // command length
+					ext = MSG_ReadUint8( msg );  // extension id
+					MSG_ReadUint8( msg );        // version number
+					len = MSG_ReadInt16( msg ); // command length
 
 					switch( ext ) {
 						default:

source/client/cl_serverlist.c
@@ -407,7 +407,7 @@ static void CL_ParseGetServersResponseMessage( msg_t *msg, bool extended ) {
 	netadr_t adr;
 
 	MSG_BeginReading( msg );
-	MSG_ReadLong( msg ); // skip the -1
+	MSG_ReadInt32( msg ); // skip the -1
 
 	//jump over the command name
 	header = ( extended ? "getserversExtResponse" : "getserversResponse" );
@@ -417,19 +417,19 @@ static void CL_ParseGetServersResponseMessage( msg_t *msg, bool extended ) {
 	}
 
 	while( msg->readcount + 7 <= msg->cursize ) {
-		char prefix = MSG_ReadChar( msg );
+		char prefix = MSG_ReadInt8( msg );
 
 		switch( prefix ) {
 			case '\\':
 				MSG_ReadData( msg, addr, 4 );
-				port = ShortSwap( MSG_ReadShort( msg ) ); // both endians need this swapped.
+				port = ShortSwap( MSG_ReadInt16( msg ) ); // both endians need this swapped.
 				Q_snprintfz( adrString, sizeof( adrString ), "%u.%u.%u.%u:%u", addr[0], addr[1], addr[2], addr[3], port );
 				break;
 
 			case '/':
 				if( extended ) {
 					MSG_ReadData( msg, addr, 16 );
-					port = ShortSwap( MSG_ReadShort( msg ) ); // both endians need this swapped.
+					port = ShortSwap( MSG_ReadInt16( msg ) ); // both endians need this swapped.
 					Q_snprintfz( adrString, sizeof( adrString ), "[%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x]:%hu",
 								 addr[ 0], addr[ 1], addr[ 2], addr[ 3], addr[ 4], addr[ 5], addr[ 6], addr[ 7],
 								 addr[ 8], addr[ 9], addr[10], addr[11], addr[12], addr[13], addr[14], addr[15],

source/game/ai/ai.cpp
@@ -87,7 +87,6 @@ void AITools_DrawLine( const vec3_t origin, const vec3_t dest ) {
 	edict_t *event;
 
 	event = G_SpawnEvent( EV_GREEN_LASER, 0, const_cast<float *>( origin ) );
-	event->r.svflags = SVF_TRANSMITORIGIN2;
 	VectorCopy( dest, event->s.origin2 );
 	G_SetBoundsForSpanEntity( event, 8 );
 	GClip_LinkEntity( event );
@@ -98,7 +97,6 @@ void AITools_DrawColorLine( const vec3_t origin, const vec3_t dest, int color, i
 
 	event = G_SpawnEvent( EV_PNODE, parm, const_cast<float *>( origin ) );
 	event->s.colorRGBA = color;
-	event->r.svflags = SVF_TRANSMITORIGIN2;
 	VectorCopy( dest, event->s.origin2 );
 	G_SetBoundsForSpanEntity( event, 8 );
 	GClip_LinkEntity( event );

commit 65d7cce192ab5fde0132c7edf4b10151d0fba03e

Un-hide the laser beam
Fix regression introduced in 82ce50b5a437020c8dba83c4e78ee2fa3a098e6b

source/game/g_weapon.cpp
@@ -1405,6 +1405,7 @@ static edict_t *_FindOrSpawnLaser( edict_t *owner, int entType, bool *newLaser )
 		laser->movetype = MOVETYPE_NONE;
 		laser->r.solid = SOLID_NOT;
 		laser->s.modelindex = 255; // needs to have some value so it isn't filtered by the server culling
+		laser->r.svflags &= ~SVF_NOCLIENT;
 	}
 
 	return laser;