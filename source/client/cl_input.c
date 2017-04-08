Qfusion/qfusion

commit 9f6fa32a3ac65f5de29f027bdfbbb28a7e8f898d

Convert forward/side/up moves from floats to signed chars

source/client/cl_input.c
@@ -211,9 +211,9 @@ static void CL_SetUcmdMovement( usercmd_t *ucmd ) {
 		CL_GameModule_AddMovement( movement );
 	}
 
-	ucmd->sidemove = bound( -1.0f, movement[0], 1.0f );
-	ucmd->forwardmove = bound( -1.0f, movement[1], 1.0f );
-	ucmd->upmove = bound( -1.0f, movement[2], 1.0f );
+	ucmd->sidemove = bound( -127, movement[0] * 127, 127 );
+	ucmd->forwardmove = bound( -127, movement[1] * 127, 127 );
+	ucmd->upmove = bound( -127, movement[2] * 127, 127 );
 }
 
 /*
@@ -257,11 +257,6 @@ static void CL_RefreshUcmd( usercmd_t *ucmd, int msec, bool ready ) {
 		if( ucmd->msec < 1 ) {
 			ucmd->msec = 1;
 		}
-
-		// snap push fracs so client and server version match
-		ucmd->forwardmove = ( (int)( UCMD_PUSHFRAC_SNAPSIZE * ucmd->forwardmove ) ) / UCMD_PUSHFRAC_SNAPSIZE;
-		ucmd->sidemove = ( (int)( UCMD_PUSHFRAC_SNAPSIZE * ucmd->sidemove ) ) / UCMD_PUSHFRAC_SNAPSIZE;
-		ucmd->upmove = ( (int)( UCMD_PUSHFRAC_SNAPSIZE * ucmd->upmove ) ) / UCMD_PUSHFRAC_SNAPSIZE;
 	}
 
 	ucmd->angles[0] = ANGLE2SHORT( cl.viewangles[0] );

source/game/ai/bot_movement.cpp
@@ -128,13 +128,13 @@ void Bot::TryEscapeIfBlocked( usercmd_t *ucmd ) {
 
 	// Way to this point should not be blocked
 	SetPendingLookAtPoint( Vec3( aasWorld->Areas()[currAasAreaNum].center ), 1.5f );
-	ucmd->forwardmove = 1;
+	ucmd->forwardmove = 127;
 	ucmd->buttons |= BUTTON_SPECIAL;
 }
 
 void Bot::MoveOnLadder( Vec3 *intendedLookVec, usercmd_t *ucmd ) {
-	ucmd->forwardmove = 1;
-	ucmd->upmove = 1;
+	ucmd->forwardmove = 127;
+	ucmd->upmove = 127;
 	ucmd->sidemove = 0;
 }
 
@@ -157,7 +157,7 @@ void Bot::MoveTriggeredARocketJump( Vec3 *intendedLookVec, usercmd_t *ucmd ) {
 		}
 		rocketJumpMovementState.hasCorrectedRocketJump = true;
 	} else if( rocketJumpMovementState.timeoutAt - level.time < 300 ) {
-		ucmd->forwardmove = 1;
+		ucmd->forwardmove = 127;
 
 		// Bounce off walls
 		if( !closeAreaProps.leftTest.CanWalk() || !closeAreaProps.rightTest.CanWalk() ) {
@@ -274,7 +274,7 @@ void Bot::MoveEnteringJumppad( Vec3 *intendedLookVec, usercmd_t *ucmd ) {
 	for( int i = 0; i < selectedAreasCount; ++i )
 		jumppadLandingAreas[jumppadLandingAreasCount++] = areasAndTravelTimes[i].areaNum;
 
-	ucmd->forwardmove = 1;
+	ucmd->forwardmove = 127;
 	jumppadMovementState.hasEnteredJumppad = true;
 }
 
@@ -282,7 +282,7 @@ void Bot::MoveRidingJummpad( Vec3 *intendedLookVec, usercmd_t *ucmd ) {
 	// First check whether bot finally landed to some area
 	if( self->groundentity ) {
 		jumppadMovementState.hasEnteredJumppad = false;
-		ucmd->forwardmove = 1;
+		ucmd->forwardmove = 127;
 		return;
 	}
 
@@ -369,10 +369,10 @@ void Bot::TryLandOnNearbyAreas( Vec3 *intendedLookVec, usercmd_t *ucmd ) {
 	// Do not press keys each frame
 	float r;
 	if( ( r = random() ) > 0.8 ) {
-		ucmd->forwardmove = r > 0.9 ? -1 : 1;
+		ucmd->forwardmove = r > 0.9 ? -127 : 127;
 	}
 	if( ( r = random() ) > 0.8 ) {
-		ucmd->sidemove = r > 0.9 ? -1 : 1;
+		ucmd->sidemove = r > 0.9 ? -127 : 127;
 	}
 	if( random() > 0.8 ) {
 		ucmd->buttons |= BUTTON_SPECIAL;
@@ -399,7 +399,7 @@ bool Bot::TryLandOnArea( int areaNum, Vec3 *intendedLookVec, usercmd_t *ucmd ) {
 	trace_t trace;
 	G_Trace( &trace, self->s.origin, nullptr, playerbox_stand_maxs, traceEnd.Data(), self, MASK_AISOLID );
 	if( trace.fraction == 1.0f ) {
-		ucmd->forwardmove = 1;
+		ucmd->forwardmove = 127;
 		*intendedLookVec = -areaPointToBotVec;
 		return true;
 	}
@@ -433,16 +433,16 @@ void KeyMoveVecToUcmd( const Vec3 &keyMoveVec, const vec3_t actualLookDir, const
 
 	float dotForward = keyMoveVec.Dot( actualLookDir );
 	if( dotForward > 0.3 ) {
-		ucmd->forwardmove = 1;
+		ucmd->forwardmove = 127;
 	} else if( dotForward < -0.3 ) {
-		ucmd->forwardmove = -1;
+		ucmd->forwardmove = -127;
 	}
 
 	float dotRight = keyMoveVec.Dot( actualRightDir );
 	if( dotRight > 0.3 ) {
-		ucmd->sidemove = 1;
+		ucmd->sidemove = 127;
 	} else if( dotRight < -0.3 ) {
-		ucmd->sidemove = -1;
+		ucmd->sidemove = -127;
 	}
 }
 
@@ -455,7 +455,7 @@ void Bot::MoveCampingASpotWithGivenLookAtPoint( const Vec3 &lookAtPoint, Vec3 *i
 
 	if( distance / campingSpotState.spotRadius > 2.0f ) {
 		// Bot should return to a point
-		ucmd->forwardmove = 1;
+		ucmd->forwardmove = 127;
 		ucmd->sidemove = 0;
 		ucmd->upmove = 0;
 
@@ -531,11 +531,11 @@ void Bot::MoveOnPlatform( Vec3 *intendedLookVec, usercmd_t *ucmd ) {
 }
 
 void Bot::MoveSwimming( Vec3 *intendedLookVec, usercmd_t *ucmd ) {
-	ucmd->forwardmove = true;
+	ucmd->forwardmove = 127;
 
 	// TODO: Check reachibility, if we are close, exit water
 	//if (!(G_PointContents(nodes[self->ai->next_node].origin) & MASK_WATER))  // Exit water
-	//    ucmd->upmove = 1;
+	//    ucmd->upmove = 127;
 }
 
 constexpr float Z_NO_BEND_SCALE = 0.25f;
@@ -574,7 +574,7 @@ bool Bot::CheckAndTryAvoidObstacles( Vec3 *intendedLookVec, usercmd_t *ucmd, flo
 		trace_t crouchTrace;
 		G_Trace( &crouchTrace, self->s.origin, nullptr, playerbox_crouch_maxs, forwardPoint.Data(), self, MASK_AISOLID );
 		if( crouchTrace.fraction == 1.0f ) {
-			ucmd->upmove = -1;
+			ucmd->upmove = -127;
 			return true;
 		}
 	}
@@ -881,7 +881,7 @@ void Bot::ApplyPendingLandingDash( usercmd_t *ucmd ) {
 		return;
 	}
 
-	ucmd->forwardmove = 1;
+	ucmd->forwardmove = 127;
 	ucmd->sidemove = 0;
 	ucmd->upmove = 0;
 	ucmd->buttons |= BUTTON_SPECIAL;
@@ -921,7 +921,7 @@ void Bot::MoveGenericRunning( Vec3 *intendedLookVec, usercmd_t *ucmd, bool beSil
 	if( speed2DSquared > 0.1f ) {
 		velocityDir2D *= Q_RSqrt( speed2DSquared );
 
-		ucmd->forwardmove = 1;
+		ucmd->forwardmove = 127;
 
 		if( movementFeatures & PMFEAT_DASH ) {
 			if( speed < movementSettings[PM_STAT_DASHSPEED] ) {
@@ -932,13 +932,13 @@ void Bot::MoveGenericRunning( Vec3 *intendedLookVec, usercmd_t *ucmd, bool beSil
 			}
 			// If we are not crouching in air to prevent bumping a ceiling, keep jump key pressed
 			else if( ucmd->upmove != -1 && movementFeatures & PMFEAT_JUMP ) {
-				ucmd->upmove = 1;
+				ucmd->upmove = 127;
 			}
 		} else {
 			if( speed < movementSettings[PM_STAT_MAXSPEED] ) {
 				ucmd->upmove = 0;
 			} else if( ucmd->upmove != -1 && movementFeatures & PMFEAT_JUMP ) {
-				ucmd->upmove = 1;
+				ucmd->upmove = 127;
 			}
 		}
 
@@ -981,20 +981,20 @@ void Bot::MoveGenericRunning( Vec3 *intendedLookVec, usercmd_t *ucmd, bool beSil
 					float targetDirDotRight = toTargetDir2D.Dot( rightLookDir );
 
 					if( targetDirDotForward > 0.3f ) {
-						ucmd->forwardmove = 1;
+						ucmd->forwardmove = 127;
 					} else if( targetDirDotForward < -0.3f ) {
-						ucmd->forwardmove = -1;
+						ucmd->forwardmove = -127;
 					}
 
 					if( targetDirDotRight > 0.3f ) {
-						ucmd->sidemove = 1;
+						ucmd->sidemove = 127;
 					} else if( targetDirDotRight > 0.3f ) {
-						ucmd->sidemove = -1;
+						ucmd->sidemove = -127;
 					}
 
 					// Prevent blocking if neither forwardmove, not sidemove has been chosen
 					if( !ucmd->forwardmove && !ucmd->sidemove ) {
-						ucmd->forwardmove = -1;
+						ucmd->forwardmove = -127;
 					}
 				} else if( velocityToTarget2DDot < 0.1f ) {
 					if( !beSilent && MaySetPendingLandingDash() ) {
@@ -1465,7 +1465,7 @@ void Bot::TriggerWeaponJump( usercmd_t *ucmd, const Vec3 &targetOrigin, const Ve
 
 	ucmd->forwardmove = 0;
 	ucmd->sidemove = 0;
-	ucmd->upmove = 1;
+	ucmd->upmove = 127;
 	ucmd->buttons |= ( BUTTON_ATTACK | BUTTON_SPECIAL );
 
 	rocketJumpMovementState.SetTriggered( targetOrigin, fireTarget, 750 );
@@ -1574,9 +1574,9 @@ void Bot::CombatMovement( usercmd_t *ucmd, bool hasToEvade ) {
 		UpdateCombatMovePushes();
 	}
 
-	ucmd->forwardmove = combatMovePushes[0];
-	ucmd->sidemove = combatMovePushes[1];
-	ucmd->upmove = combatMovePushes[2];
+	ucmd->forwardmove = bound( -127, combatMovePushes[0] * 127, 127 );
+	ucmd->sidemove = bound( -127, combatMovePushes[1] * 127, 127 );
+	ucmd->upmove = bound( -127, combatMovePushes[2] * 127, 127 );
 
 	// Dash is a single-frame event not affected by friction, so it should be checked each frame
 	if( MayApplyCombatDash() ) {
@@ -1593,7 +1593,7 @@ void Bot::CombatMovement( usercmd_t *ucmd, bool hasToEvade ) {
 
 			// If bot would not dash, do at least a jump
 			if( squareGroundSpeed > 0.64f * maxGroundSpeed * maxGroundSpeed ) {
-				ucmd->upmove = 1;
+				ucmd->upmove = 127;
 			}
 		}
 	} else {

source/qcommon/msg.c
@@ -421,9 +421,6 @@ void MSG_WriteField( msg_t *msg, const void *to, const msg_field_t *field ) {
 		case MSG_ENCTYPE_FLOAT:
 			MSG_WriteFloat( msg, *((float *)( (uint8_t *)to + field->offset )) );
 			break;
-		case MSG_ENCTYPE_FLOAT8:
-			MSG_WriteInt8( msg, (int)((*((float *)( (uint8_t *)to + field->offset ))) * 127.0f) );
-			break;
 		case MSG_ENCTYPE_FLOAT88:
 			MSG_WriteInt16( msg, (int)((*((float *)( (uint8_t *)to + field->offset ))) * 255.0f) );
 			break;
@@ -568,9 +565,6 @@ void MSG_ReadField( msg_t *msg, const void *to, const msg_field_t *field ) {
 		break;
 	case MSG_ENCTYPE_FLOAT:
 		*((float *)( (uint8_t *)to + field->offset )) = MSG_ReadFloat( msg );
-		break;
-	case MSG_ENCTYPE_FLOAT8:
-		*((float *)( (uint8_t *)to + field->offset )) = (float)MSG_ReadInt8( msg ) / 127.0f;
 		break;
 	case MSG_ENCTYPE_FLOAT88:
 		*((float *)( (uint8_t *)to + field->offset )) = (float)MSG_ReadInt16( msg ) / 255.0f;
@@ -880,9 +874,9 @@ static const msg_field_t usercmd_fields[] = {
 	{ UCOFS( angles[1] ), 16, MSG_ENCTYPE_FIXEDINT16 },
 	{ UCOFS( angles[2] ), 16, MSG_ENCTYPE_FIXEDINT16 },
 
-	{ UCOFS( forwardmove ), 0, MSG_ENCTYPE_FLOAT8 },
-	{ UCOFS( sidemove ), 0, MSG_ENCTYPE_FLOAT8 },
-	{ UCOFS( upmove ), 0, MSG_ENCTYPE_FLOAT8 },
+	{ UCOFS( forwardmove ), 0, MSG_ENCTYPE_FIXEDINT8 },
+	{ UCOFS( sidemove ), 0, MSG_ENCTYPE_FIXEDINT8 },
+	{ UCOFS( upmove ), 0, MSG_ENCTYPE_FIXEDINT8 },
 
 	{ UCOFS( buttons ), 8, MSG_ENCTYPE_UBASE128 },
 };

source/cgame/cg_democams.cpp
@@ -954,9 +954,9 @@ int CG_DemoCam_FreeFly( void ) {
 		AngleVectors( moveangles, forward, right, up );
 		VectorCopy( moveangles, cam_angles );
 
-		fmove = cmd.forwardmove * SPEED;
-		smove = cmd.sidemove * SPEED;
-		upmove = cmd.upmove * SPEED;
+		fmove = cmd.forwardmove * SPEED / 127.0f;
+		smove = cmd.sidemove * SPEED / 127.0f;
+		upmove = cmd.upmove * SPEED / 127.0f;
 		if( cmd.buttons & BUTTON_SPECIAL ) {
 			maxspeed *= 2;
 		}

source/gameshared/gs_pmove.c
@@ -40,7 +40,7 @@ vec3_t playerbox_gib_mins = { -16, -16, 0 };
 vec3_t playerbox_gib_maxs = { 16, 16, 16 };
 int playerbox_gib_viewheight = 8;
 
-#define SPEEDKEY    500
+#define SPEEDKEY    500.0f
 
 #define PM_DASHJUMP_TIMEDELAY 1000 // delay in milliseconds
 #define PM_WALLJUMP_TIMEDELAY   1300
@@ -1906,9 +1906,9 @@ void Pmove( pmove_t *pmove ) {
 		}
 	}
 
-	pml.forwardPush = pm->cmd.forwardmove * SPEEDKEY;
-	pml.sidePush = pm->cmd.sidemove * SPEEDKEY;
-	pml.upPush = pm->cmd.upmove * SPEEDKEY;
+	pml.forwardPush = pm->cmd.forwardmove * SPEEDKEY / 127.0f;
+	pml.sidePush = pm->cmd.sidemove * SPEEDKEY / 127.0f;
+	pml.upPush = pm->cmd.upmove * SPEEDKEY / 127.0f;
 
 	if( pm->playerState->pmove.stats[PM_STAT_NOUSERCONTROL] > 0 ) {
 		pml.forwardPush = 0;

source/gameshared/q_comref.h
@@ -54,15 +54,13 @@ enum {
 #define CMD_BACKUP  64  // allow a lot of command backups for very fast systems
 #define CMD_MASK    ( CMD_BACKUP - 1 )
 
-#define UCMD_PUSHFRAC_SNAPSIZE 127.0f //32767.0f//send as char or short
-
 // usercmd_t is sent to the server each client frame
 typedef struct usercmd_s {
 	uint8_t msec;
 	uint8_t buttons;
-	short angles[3];
-	float forwardmove, sidemove, upmove;
-	unsigned int serverTimeStamp;
+	int16_t angles[3];
+	int8_t forwardmove, sidemove, upmove;
+	uint32_t serverTimeStamp;
 } usercmd_t;
 
 #define MAX_PM_STATS 16
@@ -275,7 +273,6 @@ typedef enum MSG_ENCTYPE_e {
 	MSG_ENCTYPE_FIXEDINT64,				// 64-bit integer
 
 	MSG_ENCTYPE_FLOAT,					// 32-bit float
-	MSG_ENCTYPE_FLOAT8,					// 32-bit float value represented as a 8-bit signed integer (should be in -1..1 range)
 	MSG_ENCTYPE_FLOAT88,				// 32-bit float value represented as a 8.8 fixed-point integer
 
 	MSG_ENCTYPE_COORD24,				// 32-bit float value represented as a 20.4 fixed-point integer

source/tv_server/tv_module/tvm_pmove.c
@@ -50,7 +50,7 @@ const float pm_friction = 8; // initially 6
 const float pm_accelerate = 12; // user intended acceleration when on ground or fly movement ( initially 10 )
 const float pm_decelerate = 12; // user intended deceleration when on ground
 
-#define SPEEDKEY    500
+#define SPEEDKEY    500.0f
 
 /*
 * TVM_PM_ClampAngles
@@ -238,9 +238,9 @@ void TVM_Pmove( pmove_t *pmove ) {
 
 	pm->playerState->viewheight = playerbox_stand_viewheight;
 
-	pml.forwardPush = pm->cmd.forwardmove * SPEEDKEY;
-	pml.sidePush = pm->cmd.sidemove * SPEEDKEY;
-	pml.upPush = pm->cmd.upmove * SPEEDKEY;
+	pml.forwardPush = pm->cmd.forwardmove * SPEEDKEY / 127.0f;
+	pml.sidePush = pm->cmd.sidemove * SPEEDKEY / 127.0f;
+	pml.upPush = pm->cmd.upmove * SPEEDKEY / 127.0f;
 
 	if( pm->playerState->pmove.stats[PM_STAT_NOUSERCONTROL] > 0 ) {
 		pml.forwardPush = 0;

commit e74a78f2f3b98101e9187dc99365d4c38a1ac3ef

Fix bugs introduced in 9f6fa32a3ac65

source/client/cl_input.c
@@ -211,9 +211,9 @@ static void CL_SetUcmdMovement( usercmd_t *ucmd ) {
 		CL_GameModule_AddMovement( movement );
 	}
 
-	ucmd->sidemove = bound( -127, movement[0] * 127, 127 );
-	ucmd->forwardmove = bound( -127, movement[1] * 127, 127 );
-	ucmd->upmove = bound( -127, movement[2] * 127, 127 );
+	ucmd->sidemove = bound( -127, (int)(movement[0] * 127.0f), 127 );
+	ucmd->forwardmove = bound( -127, (int)(movement[1] * 127.0f), 127 );
+	ucmd->upmove = bound( -127, (int)(movement[2] * 127.0f), 127 );
 }
 
 /*

source/game/ai/bot_movement.cpp
@@ -1612,8 +1612,8 @@ void Bot::ApplyCheatingGroundAcceleration( const usercmd_t *ucmd ) {
 	float frameTimeSeconds = 0.0001f * game.frametime;
 	float factor = speedGainPerSecond * frameTimeSeconds;
 
-	VectorMA( self->velocity, factor * ucmd->forwardmove, forward, self->velocity );
-	VectorMA( self->velocity, factor * ucmd->sidemove, right, self->velocity );
+	VectorMA( self->velocity, factor * ucmd->forwardmove / 127.0f, forward, self->velocity );
+	VectorMA( self->velocity, factor * ucmd->sidemove / 127.0f, right, self->velocity );
 
 	float squareSpeed = VectorLengthSquared( self->velocity );
 	if( squareSpeed > 1 ) {

source/qcommon/msg.c
@@ -1098,9 +1098,9 @@ static const msg_field_t usercmd_fields[] = {
 	{ UCOFS( angles[1] ), 16, 1, MSG_ENCTYPE_FIXED_INT16 },
 	{ UCOFS( angles[2] ), 16, 1, MSG_ENCTYPE_FIXED_INT16 },
 
-	{ UCOFS( forwardmove ), 0, 1, MSG_ENCTYPE_FIXED_INT8 },
-	{ UCOFS( sidemove ), 0, 1, MSG_ENCTYPE_FIXED_INT8 },
-	{ UCOFS( upmove ), 0, 1, MSG_ENCTYPE_FIXED_INT8 },
+	{ UCOFS( forwardmove ), 8, 1, MSG_ENCTYPE_FIXED_INT8 },
+	{ UCOFS( sidemove ), 8, 1, MSG_ENCTYPE_FIXED_INT8 },
+	{ UCOFS( upmove ), 8, 1, MSG_ENCTYPE_FIXED_INT8 },
 
 	{ UCOFS( buttons ), 32, 1, MSG_ENCTYPE_UBASE128 },
 };