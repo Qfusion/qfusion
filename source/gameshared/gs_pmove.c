4ae801352a7e9556e29f57c57de899fef0b7dcb7
dnk777 <dnk777@users.noreply.github.com>
2017-07-14 19:21:59 +0300

    Add a hint flag and several out fields to pmove_t
    * Add a flag to skip collision tests while executing Pmove().
      This flag is intended to be used in bot movement prediction.
    
    * Add "ladder", "groundplane", "groundsurfFlags",
      "groundcontents" pmove_t fields set by a Pmove() call.
      These fields are useful for future bot development.

--- source/gameshared/gs_pmove.c
+++ source/gameshared/gs_pmove.c
@@ -303,6 +303,12 @@ static int PM_SlideMove( void ) {
 		}
 	}
 
+	// Do a shortcut in this case
+	if( pm->skipCollision ) {
+		VectorMA( pml.origin, remainingTime, pml.velocity, pml.origin );
+		return blockedmask;
+	}
+
 	VectorCopy( pml.velocity, old_velocity );
 	VectorCopy( pml.origin, last_valid_origin );
 
@@ -443,6 +449,12 @@ static void PM_StepSlideMove( void ) {
 	VectorCopy( pml.velocity, start_v );
 
 	blocked = PM_SlideMove();
+
+	// We have modified the origin in PM_SlideMove() in this case.
+	// No further computations are required.
+	if( pm->skipCollision ) {
+		return;
+	}
 
 	VectorCopy( pml.origin, down_o );
 	VectorCopy( pml.velocity, down_v );
@@ -951,6 +963,12 @@ static void PM_Move( void ) {
 static void PM_GroundTrace( trace_t *trace ) {
 	vec3_t point;
 
+	if( pm->skipCollision ) {
+		memset( trace, 0, sizeof( trace_t ) );
+		trace->fraction = 1.0f;
+		return;
+	}
+
 	// see if standing on something solid
 	point[0] = pml.origin[0];
 	point[1] = pml.origin[1];
@@ -1032,6 +1050,9 @@ static void PM_CategorizePosition( void ) {
 			pm->playerState->pmove.pm_flags &= ~PMF_ON_GROUND;
 		} else {
 			pm->groundentity = trace.ent;
+			pm->groundplane = trace.plane;
+			pm->groundsurfFlags = trace.surfFlags;
+			pm->groundcontents = trace.contents;
 
 			// hitting solid ground will end a waterjump
 			if( pm->playerState->pmove.pm_flags & PMF_TIME_WATERJUMP ) {
@@ -1293,6 +1314,7 @@ static void PM_CheckWallJump( void ) {
 		&& ( pm->playerState->pmove.stats[PM_STAT_FEATURES] & PMFEAT_WALLJUMP ) &&
 		( !( pm->playerState->pmove.pm_flags & PMF_WALLJUMPCOUNT ) )
 		&& pm->playerState->pmove.stats[PM_STAT_WJTIME] <= 0
+		&& !pm->skipCollision
 		) {
 		trace_t trace;
 		vec3_t point;
@@ -1406,6 +1428,8 @@ static void PM_CheckSpecialMovement( void ) {
 	int cont;
 	trace_t trace;
 
+	pm->ladder = false;
+
 	if( pm->playerState->pmove.pm_time ) {
 		return;
 	}
@@ -1413,10 +1437,13 @@ static void PM_CheckSpecialMovement( void ) {
 	pml.ladder = false;
 
 	// check for ladder
-	VectorMA( pml.origin, 1, pml.flatforward, spot );
-	module_Trace( &trace, pml.origin, pm->mins, pm->maxs, spot, pm->playerState->POVnum, pm->contentmask, 0 );
-	if( ( trace.fraction < 1 ) && ( trace.surfFlags & SURF_LADDER ) ) {
-		pml.ladder = true;
+	if( !pm->skipCollision ) {
+		VectorMA( pml.origin, 1, pml.flatforward, spot );
+		module_Trace( &trace, pml.origin, pm->mins, pm->maxs, spot, pm->playerState->POVnum, pm->contentmask, 0 );
+		if( ( trace.fraction < 1 ) && ( trace.surfFlags & SURF_LADDER ) ) {
+			pml.ladder = true;
+			pm->ladder = true;
+		}
 	}
 
 	// check for water jump
