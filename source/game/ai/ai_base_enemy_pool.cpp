0850d5e2aa52283e4f38bc7e7ea681147dc5095c
dnk777 <dnk777@users.noreply.github.com>
2017-08-16 20:11:18 +0300

    Guess an enemy origin by a detected Danger
    Also:
    
    * Check teammates origins while guessing enemy one
    
    * Try guessing enemy origin from any shot entity

--- source/game/ai/ai_base_enemy_pool.cpp
+++ source/game/ai/ai_base_enemy_pool.cpp
@@ -51,13 +51,15 @@ void Enemy::Clear() {
 	lastSeenAt = 0;
 }
 
-void Enemy::OnViewed() {
+void Enemy::OnViewed( const float *specifiedOrigin ) {
 	if( lastSeenSnapshots.size() == MAX_TRACKED_POSITIONS ) {
 		lastSeenSnapshots.pop_front();
 	}
 
+	// Put the likely case first
+	const float *origin = !specifiedOrigin ? ent->s.origin : specifiedOrigin;
 	// Set members for faster access
-	VectorCopy( ent->s.origin, lastSeenPosition.Data() );
+	VectorCopy( origin, lastSeenPosition.Data() );
 	VectorCopy( ent->velocity, lastSeenVelocity.Data() );
 	lastSeenAt = level.time;
 	// Store in a queue then for history
@@ -544,7 +546,44 @@ void AiBaseEnemyPool::OnEnemyViewed( const edict_t *enemy ) {
 		trackedEnemiesCount++;
 	} else {
 		Debug( "has viewed a new enemy %s, all slots are used. Should try evict some slot\n", Nick( enemy ) );
-		TryPushNewEnemy( enemy );
+		TryPushNewEnemy( enemy, nullptr );
+	}
+}
+
+void AiBaseEnemyPool::OnEnemyOriginGuessed( const edict_t *enemy,
+											unsigned minMillisSinceLastSeen,
+											const float *guessedOrigin ) {
+	if( !enemy ) {
+		return;
+	}
+
+	const int64_t levelTime = level.time;
+	int freeSlot = -1;
+	for( unsigned i = 0; i < maxTrackedEnemies; ++i ) {
+		if( !trackedEnemies[i].ent ) {
+			freeSlot = i;
+			continue;
+		}
+		if( trackedEnemies[i].ent != enemy ) {
+			continue;
+		}
+		// If there is already an Enemy record containing an entity,
+		// check whether this record timed out enough to be overwritten.
+		// This code prevents overwriting
+		if( trackedEnemies[i].lastSeenAt + minMillisSinceLastSeen > levelTime ) {
+			continue;
+		}
+		trackedEnemies[i].OnViewed();
+		return;
+	}
+
+	if( freeSlot > 0 ) {
+		Debug( "has guessed a new origin for not seen enemy %s, uses a free slot to remember it\n", Nick( enemy ) );
+		EmplaceEnemy( enemy, freeSlot, guessedOrigin );
+		trackedEnemiesCount++;
+	} else {
+		Debug( "has guessed a new enemy %s, all slots are used. Should try to evict some slot\n", Nick( enemy ) );
+		TryPushNewEnemy( enemy, guessedOrigin );
 	}
 }
 
@@ -569,7 +608,7 @@ void AiBaseEnemyPool::RemoveEnemy( Enemy &enemy ) {
 	--trackedEnemiesCount;
 }
 
-void AiBaseEnemyPool::EmplaceEnemy( const edict_t *enemy, int slot ) {
+void AiBaseEnemyPool::EmplaceEnemy( const edict_t *enemy, int slot, const float *specifiedOrigin ) {
 	Enemy &slotEnemy = trackedEnemies[slot];
 	slotEnemy.ent = enemy;
 	slotEnemy.registeredAt = level.time;
@@ -577,11 +616,11 @@ void AiBaseEnemyPool::EmplaceEnemy( const edict_t *enemy, int slot ) {
 	slotEnemy.avgPositiveWeight = 0.0f;
 	slotEnemy.maxPositiveWeight = 0.0f;
 	slotEnemy.positiveWeightsCount = 0;
-	slotEnemy.OnViewed();
+	slotEnemy.OnViewed( specifiedOrigin );
 	Debug( "has stored enemy %s in slot %d\n", slotEnemy.Nick(), slot );
 }
 
-void AiBaseEnemyPool::TryPushEnemyOfSingleBot( const edict_t *bot, const edict_t *enemy ) {
+void AiBaseEnemyPool::TryPushEnemyOfSingleBot( const edict_t *bot, const edict_t *enemy, const float *specifiedOrigin ) {
 	// Try to find a free slot. For each used and not reserved slot compute eviction score relative to new enemy
 	int candidateSlot = -1;
 
@@ -591,7 +630,12 @@ void AiBaseEnemyPool::TryPushEnemyOfSingleBot( const edict_t *bot, const edict_t
 	// Significantly increases chances to get a slot, but not guarantees it.
 	bool isNewEnemyAttacker = LastAttackedByTime( enemy ) > 0;
 	// It will be useful inside the loop, so it needs to be precomputed
-	float distanceToNewEnemy = ( Vec3( bot->s.origin ) - enemy->s.origin ).LengthFast();
+	float distanceToNewEnemy;
+	if( !specifiedOrigin ) {
+		distanceToNewEnemy = ( Vec3( bot->s.origin ) - enemy->s.origin ).LengthFast();
+	} else {
+		distanceToNewEnemy = DistanceFast( bot->s.origin, specifiedOrigin );
+	}
 	float newEnemyWeight = ComputeRawEnemyWeight( enemy );
 
 	for( unsigned i = 0; i < maxTrackedEnemies; ++i ) {
@@ -652,7 +696,7 @@ void AiBaseEnemyPool::TryPushEnemyOfSingleBot( const edict_t *bot, const edict_t
 
 	if( candidateSlot != -1 ) {
 		Debug( "will evict %s to make a free slot, new enemy have higher priority atm\n", Nick( enemy ) );
-		EmplaceEnemy( enemy, candidateSlot );
+		EmplaceEnemy( enemy, candidateSlot, specifiedOrigin );
 	} else {
 		Debug( "can't find free slot for %s, all current enemies have higher priority\n", Nick( enemy ) );
 	}
--- source/game/ai/ai_base_enemy_pool.h
+++ source/game/ai/ai_base_enemy_pool.h
@@ -127,7 +127,7 @@ public:
 	static constexpr unsigned MAX_TRACKED_POSITIONS = 16;
 
 	void Clear();
-	void OnViewed();
+	void OnViewed( const float *specifiedOrigin = nullptr );
 
 	inline const char *Nick() const {
 		if( !ent ) {
@@ -302,7 +302,7 @@ private:
 	StaticVector<AttackStats, MAX_TRACKED_ATTACKERS> attackers;
 	StaticVector<AttackStats, MAX_TRACKED_TARGETS> targets;
 
-	void EmplaceEnemy( const edict_t *enemy, int slot );
+	void EmplaceEnemy( const edict_t *enemy, int slot, const float *specifiedOrigin = nullptr );
 	void RemoveEnemy( Enemy &enemy );
 
 	void UpdateEnemyWeight( Enemy &enemy );
@@ -324,7 +324,7 @@ protected:
 	// Used to compare enemy strength and pool owner
 	virtual float ComputeDamageToBeKilled() const = 0;
 	// Contains enemy eviction code
-	virtual void TryPushNewEnemy( const edict_t *enemy ) = 0;
+	virtual void TryPushNewEnemy( const edict_t *enemy, const float *suggesedOrigin ) = 0;
 	// Overridden method may give some additional weight to an enemy
 	// (Useful for case when a bot should have some reinforcements)
 	virtual float GetAdditionalEnemyWeight( const edict_t *bot, const edict_t *enemy ) const = 0;
@@ -342,7 +342,7 @@ protected:
 	inline float AvgSkill() const { return avgSkill; }
 	inline float DecisionRandom() const { return decisionRandom; }
 
-	void TryPushEnemyOfSingleBot( const edict_t *bot, const edict_t *enemy );
+	void TryPushEnemyOfSingleBot( const edict_t *bot, const edict_t *enemy, const float *specfiedOrigin = nullptr );
 
 public:
 	AiBaseEnemyPool( float avgSkill_ );
@@ -366,6 +366,9 @@ public:
 	}
 
 	void OnEnemyViewed( const edict_t *enemy );
+	void OnEnemyOriginGuessed( const edict_t *enemy,
+							   unsigned minMillisSinceLastSeen,
+							   const float *guessedOrigin = nullptr );
 
 	// Force the pool to forget the enemy (for example, when bot attitude to an enemy has been changed)
 	void Forget( const edict_t *enemy );
