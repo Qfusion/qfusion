7596da8a935ee8139066d33461e18c2dbfb83a0c
Victor Luchits <vluchits@gmail.com>
2017-10-09 01:48:32 +0300

    Do not use half-precision floats for stream VBOs
    Converting to a half float is a very expensive operation and dynamic meshes can pile up to thousands of vertices per frame
    
    This reverts 42e3a59e6b8e327fb09ecfb524a4fc57970993ff

--- source/ref_gl/r_backend.c
+++ source/ref_gl/r_backend.c
@@ -662,7 +662,7 @@ void RB_RegisterStreamVBOs( void ) {
 		}
 		stream->vbo = R_CreateMeshVBO( &rb,
 									   MAX_STREAM_VBO_VERTS, MAX_STREAM_VBO_ELEMENTS, 0,
-									   vattribs[i], VBO_TAG_STREAM, VATTRIB_TEXCOORDS_BIT | VATTRIB_NORMAL_BIT | VATTRIB_SVECTOR_BIT );
+									   vattribs[i], VBO_TAG_STREAM, 0 );
 		stream->vertexData = RB_Alloc( MAX_STREAM_VBO_VERTS * stream->vbo->vertexSize );
 	}
 }
