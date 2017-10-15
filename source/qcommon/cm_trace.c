20ba39e2e89ba4cfa69836ddf89524a3d85f2a1f
Victor Luchits <vluchits@gmail.com>
2017-10-15 14:41:15 +0300

    Fix entity hulls to use enlarged bogus bounds
    Using world bounds wasn't a proper solution because CM_TransformedBoxTrace is performed in entity space

--- source/qcommon/cm_trace.c
+++ source/qcommon/cm_trace.c
@@ -36,9 +36,9 @@ void CM_InitBoxHull( cmodel_state_t *cms ) {
 	cms->box_brush->numsides = 6;
 	cms->box_brush->brushsides = cms->box_brushsides;
 	cms->box_brush->contents = CONTENTS_BODY;
+
 	// Make sure CM_CollideBox() will not reject the brush by its bounds
-	VectorCopy( cms->world_mins, cms->box_brush->mins );
-	VectorCopy( cms->world_maxs, cms->box_brush->maxs );
+	ClearBounds( cms->box_brush->maxs, cms->box_brush->mins );
 
 	cms->box_markbrushes[0] = cms->box_brush;
 
@@ -90,9 +90,9 @@ void CM_InitOctagonHull( cmodel_state_t *cms ) {
 	cms->oct_brush->numsides = 10;
 	cms->oct_brush->brushsides = cms->oct_brushsides;
 	cms->oct_brush->contents = CONTENTS_BODY;
+
 	// Make sure CM_CollideBox() will not reject the brush by its bounds
-	VectorCopy( cms->world_mins, cms->oct_brush->mins );
-	VectorCopy( cms->world_maxs, cms->oct_brush->maxs );
+	ClearBounds( cms->box_brush->maxs, cms->box_brush->mins );
 
 	cms->oct_markbrushes[0] = cms->oct_brush;
 
