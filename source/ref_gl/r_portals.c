bfdaf80823b4b894ffc82921d7dbd5fd04e134c5
Victor Luchits <vluchits@gmail.com>
2018-05-09 19:49:28 +0300

    Fix sky portals

--- source/ref_gl/r_portals.c
+++ source/ref_gl/r_portals.c
@@ -493,7 +493,7 @@ portalSurface_t *R_AddSkyportalSurface( const entity_t *ent, const shader_t *sha
 		rn.numDepthPortalSurfaces++;
 	}
 
-	R_AddSurfToDrawList( rn.portalmasklist, ent, rsh.skyShader, NULL, -1, 0, 0, NULL, drawSurf );
+	R_AddSurfToDrawList( rn.portalmasklist, ent, rsh.depthOnlyShader, NULL, -1, 0, 0, NULL, drawSurf );
 
 	portalSurface->entity = ent;
 	portalSurface->shader = shader;
