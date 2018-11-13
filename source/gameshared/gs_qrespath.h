17cd4f484f5e69f29941a0c68093460d66ce37bf
ahacheers <ahacheers@gmail.com>
2018-11-13 19:24:39 +0100

    change unknown map image path

--- source/gameshared/gs_qrespath.h
+++ source/gameshared/gs_qrespath.h
@@ -146,7 +146,7 @@ extern "C" {
 // weapon firing
 #define PATH_SMOKE_PUFF         "smokePuff"
 
-#define PATH_UKNOWN_MAP_PIC             "gfx/ui/unknownmap"
+#define PATH_UKNOWN_MAP_PIC             "levelshots/unknownmap"
 
 //
 //
