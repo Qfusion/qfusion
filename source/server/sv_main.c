36609c0d842a8044dfea87479805b183e5961581
Michael Savage <mikejsavage@gmail.com>
2018-11-16 22:44:59 +0200

    sv_defaultmap dust

--- source/server/sv_main.c
+++ source/server/sv_main.c
@@ -903,7 +903,7 @@ void SV_Init( void ) {
 	sv_lastAutoUpdate = Cvar_Get( "sv_lastAutoUpdate", "0", CVAR_READONLY | CVAR_ARCHIVE );
 	sv_pure_forcemodulepk3 =    Cvar_Get( "sv_pure_forcemodulepk3", "", CVAR_LATCH );
 
-	sv_defaultmap =         Cvar_Get( "sv_defaultmap", "wbomb1", CVAR_ARCHIVE );
+	sv_defaultmap =         Cvar_Get( "sv_defaultmap", "dust", CVAR_ARCHIVE );
 	sv_reconnectlimit =     Cvar_Get( "sv_reconnectlimit", "3", CVAR_ARCHIVE );
 	sv_maxclients =         Cvar_Get( "sv_maxclients", "16", CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_LATCH );
 
