965673c037474506b25158e2bf92f897747c2dcc
ahacheers <ahacheers@gmail.com>
2018-11-23 04:31:53 +0100

    chat bubble dirt

--- source/cgame/cg_local.h
+++ source/cgame/cg_local.h
@@ -255,7 +255,6 @@ typedef struct {
 	cgs_media_handle_t *shaderNet;
 	cgs_media_handle_t *shaderBackTile;
 	cgs_media_handle_t *shaderWhiteTile;
-	cgs_media_handle_t *shaderChatBalloon;
 	cgs_media_handle_t *shaderDownArrow;
 	cgs_media_handle_t *shaderTeleportShellGfx;
 
