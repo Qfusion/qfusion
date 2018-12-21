3a9fda27105e93cc0c32d70ea252630df198e29a
Michael Savage <mikejsavage@gmail.com>
2018-12-22 01:54:03 +0200

    READONLY r_wallcolor

--- source/client/renderer/r_register.cpp
+++ source/client/renderer/r_register.cpp
@@ -353,9 +353,9 @@ static void R_Register() {
 
 	r_screenshot_fmtstr = ri.Cvar_Get( "r_screenshot_fmtstr", va_r( tmp, sizeof( tmp ), "%s%%y%%m%%d_%%H%%M%%S", APP_SCREENSHOTS_PREFIX ), CVAR_ARCHIVE );
 
-	r_drawflat = ri.Cvar_Get( "r_drawflat", "1", CVAR_ARCHIVE | CVAR_READONLY );
-	r_wallcolor = ri.Cvar_Get( "r_wallcolor", "0 0 0", CVAR_CHEAT );
-	r_floorcolor = ri.Cvar_Get( "r_floorcolor", "32 32 32", CVAR_CHEAT );
+	r_drawflat = ri.Cvar_Get( "r_drawflat", "1", CVAR_READONLY );
+	r_wallcolor = ri.Cvar_Get( "r_wallcolor", "0 0 0", CVAR_READONLY );
+	r_floorcolor = ri.Cvar_Get( "r_floorcolor", "32 32 32", CVAR_READONLY );
 
 	// make sure we rebuild our 3D texture after vid_restart
 	r_wallcolor->modified = r_floorcolor->modified = true;
