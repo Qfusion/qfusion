22b523c3461e9c7f08ef316b9f274e5ff012282e
Michael Savage <mikejsavage@gmail.com>
2019-01-03 22:34:34 +0200

    No need for stdbool.h

--- source/gameshared/q_arch.h
+++ source/gameshared/q_arch.h
@@ -37,7 +37,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdlib.h>
 #include <time.h>
 #include <stdint.h>
-#include <stdbool.h>
 #include <stddef.h>
 
 #ifndef __STDC_FORMAT_MACROS
