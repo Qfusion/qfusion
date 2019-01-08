64c4921abdf96e72d3eaf50652009ba47ef9d9f7
Michael Savage <mikejsavage@gmail.com>
2019-01-08 18:59:10 +0200

    Max hitsound at 35dmg

--- source/game/p_view.cpp
+++ source/game/p_view.cpp
@@ -202,7 +202,7 @@ void G_ClientDamageFeedback( edict_t *ent ) {
 		} else {
 			if( ent->snap.kill ) {
 				G_AddPlayerStateEvent( ent->r.client, PSEV_HIT, 4 );
-			} else if( ent->snap.damage_given >= 75 ) {
+			} else if( ent->snap.damage_given >= 70 ) {
 				G_AddPlayerStateEvent( ent->r.client, PSEV_HIT, 0 );
 			} else if( ent->snap.damage_given >= 45 ) {
 				G_AddPlayerStateEvent( ent->r.client, PSEV_HIT, 1 );
