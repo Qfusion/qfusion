8cd04e787e05c8e63517030acd5fce68b2d21a23
Michael Savage <mikejsavage@gmail.com>
2019-01-24 21:27:08 +0200

    Extern C Optimus stuff

--- source/win32/win_client.cpp
+++ source/win32/win_client.cpp
@@ -1,5 +1,5 @@
 #include <windows.h>
 
 // video drivers pick these up and make sure the game runs on the good GPU
-__declspec( dllexport ) DWORD NvOptimusEnablement = 1;
-__declspec( dllexport ) int AmdPowerXpressRequestHighPerformance = 1;
+extern "C" __declspec( dllexport ) DWORD NvOptimusEnablement = 1;
+extern "C" __declspec( dllexport ) int AmdPowerXpressRequestHighPerformance = 1;
