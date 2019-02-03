620b3af82e7c1196f8835dc25a5831f963056c65
Michael Savage <mikejsavage@gmail.com>
2019-02-03 15:44:42 +0200

    Shut up stb_vorbis warnings

--- libs/stb.lua
+++ libs/stb.lua
@@ -1,8 +1,9 @@
 lib( "stb_image", { "libs/stb/stb_image.cpp" } )
-msvc_obj_cxxflags( "libs/stb/stb_image", "/wd4244 /wd4456" )
-gcc_obj_cxxflags( "libs/stb/stb_image", "-Wno-shadow -Wno-implicit-fallthrough" )
+msvc_obj_cxxflags( "libs/stb/stb_image.cpp", "/wd4244 /wd4456" )
+gcc_obj_cxxflags( "libs/stb/stb_image.cpp", "-Wno-shadow -Wno-implicit-fallthrough" )
 
 lib( "stb_image_write", { "libs/stb/stb_image_write.cpp" } )
 
 lib( "stb_vorbis", { "libs/stb/stb_vorbis.cpp" } )
-gcc_obj_cxxflags( "libs/stb/stb_image", "-Wno-shadow -Wno-unused-value" )
+msvc_obj_cxxflags( "libs/stb/stb_vorbis.cpp", "/wd4244 /wd4245 /wd4456 /wd4457 /wd4701" )
+gcc_obj_cxxflags( "libs/stb/stb_vorbis.cpp", "-Wno-shadow -Wno-unused-value" )
