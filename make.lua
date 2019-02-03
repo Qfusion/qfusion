b5e0705c69f1da76e084cc4d7847304986bf12fd
Michael Savage <mikejsavage@gmail.com>
2019-02-03 15:41:37 +0200

    Disable "warning C4307: '*': integral constant overflow"
    MSVC complains about intermediate hash results overflowing, which is a
    normal and expected thing, so the warnings are just noise

--- make.lua
+++ make.lua
@@ -7,7 +7,7 @@ require( "libs.stb" )
 
 obj_cxxflags( ".*", "-I source -I libs" )
 
-msvc_obj_cxxflags( ".*", "/W4 /wd4100 /wd4146 /wd4189 /wd4201 /wd4324 /wd4351 /wd4127 /wd4505 /wd4530 /wd4702 /D_CRT_SECURE_NO_WARNINGS" )
+msvc_obj_cxxflags( ".*", "/W4 /wd4100 /wd4146 /wd4189 /wd4201 /wd4307 /wd4324 /wd4351 /wd4127 /wd4505 /wd4530 /wd4702 /D_CRT_SECURE_NO_WARNINGS" )
 msvc_obj_cxxflags( ".*", "/fp:fast /GR-" )
 gcc_obj_cxxflags( ".*", "-std=c++11 -static-libstdc++ -msse4.2 -ffast-math -fno-rtti -fno-strict-aliasing -fno-strict-overflow -fvisibility=hidden" )
 gcc_obj_cxxflags( ".*", "-Wall -Wextra -Wcast-align -Wvla -Wformat-security" ) -- -Wconversion
