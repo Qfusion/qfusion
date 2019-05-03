2022ab0807a0ea093c1048528ab72a1273d30977
Victor Luchits <vluchits@gmail.com>
2019-05-03 13:33:09 +0300

    Update Android project file to compile C++ files as C++17

--- platforms/android/projects/qfusion/jni/Application.mk
+++ platforms/android/projects/qfusion/jni/Application.mk
@@ -1,8 +1,8 @@
 APP_ABI := armeabi-v7a x86
 APP_CFLAGS := -ffast-math -fno-strict-aliasing -funroll-loops -std=gnu99 -Werror=return-type -Wno-typedef-redefinition
-APP_CPPFLAGS := -fexceptions -frtti -std=gnu++11
+APP_CPPFLAGS := -fexceptions -frtti -std=c++17
 APP_OPTIM := release
-APP_PLATFORM := android-16
+APP_PLATFORM := android-18
 APP_STL := c++_shared
 NDK_APP_SHORT_COMMANDS := true
 NDK_TOOLCHAIN_VERSION := clang
