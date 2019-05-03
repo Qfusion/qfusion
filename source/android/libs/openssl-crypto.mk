b972ab8d7dd2b7be4df760fe40591f29258c88e5
Victor Luchits <vluchits@gmail.com>
2019-05-03 15:05:56 +0300

    Update libsrcs

--- source/android/libs/openssl-crypto.mk
+++ source/android/libs/openssl-crypto.mk
@@ -17,18 +17,43 @@ LOCAL_SRC_FILES := \
   crypto/aes/aes_cbc.c \
   crypto/aes/aes_cfb.c \
   crypto/aes/aes_core.c \
-  crypto/aes/aes_ctr.c \
   crypto/aes/aes_ecb.c \
+  crypto/aes/aes_ige.c \
   crypto/aes/aes_misc.c \
   crypto/aes/aes_ofb.c \
   crypto/aes/aes_wrap.c \
+  crypto/aes/aes_x86core.c \
+  crypto/armcap.c \
+  crypto/cpt_err.c \
+  crypto/cryptlib.c \
+  crypto/ctype.c \
+  crypto/cversion.c \
+  crypto/dllmain.c \
+  crypto/ebcdic.c \
+  crypto/ex_data.c \
+  crypto/getenv.c \
+  crypto/init.c \
+  crypto/mem.c \
+  crypto/mem_clr.c \
+  crypto/mem_dbg.c \
+  crypto/mem_sec.c \
+  crypto/o_dir.c \
+  crypto/o_fips.c \
+  crypto/o_fopen.c \
+  crypto/o_init.c \
+  crypto/o_str.c \
+  crypto/o_time.c \
+  crypto/ppccap.c \
+  crypto/s390xcap.c \
+  crypto/sparcv9cap.c \
+  crypto/threads_none.c \
+  crypto/threads_pthread.c \
+  crypto/threads_win.c \
+  crypto/uid.c \
   crypto/asn1/a_bitstr.c \
-  crypto/asn1/a_bool.c \
-  crypto/asn1/a_bytes.c \
   crypto/asn1/a_d2i_fp.c \
   crypto/asn1/a_digest.c \
   crypto/asn1/a_dup.c \
-  crypto/asn1/a_enum.c \
   crypto/asn1/a_gentm.c \
   crypto/asn1/a_i2d_fp.c \
   crypto/asn1/a_int.c \
@@ -36,7 +61,6 @@ LOCAL_SRC_FILES := \
   crypto/asn1/a_object.c \
   crypto/asn1/a_octet.c \
   crypto/asn1/a_print.c \
-  crypto/asn1/a_set.c \
   crypto/asn1/a_sign.c \
   crypto/asn1/a_strex.c \
   crypto/asn1/a_strnid.c \
@@ -48,17 +72,18 @@ LOCAL_SRC_FILES := \
   crypto/asn1/ameth_lib.c \
   crypto/asn1/asn1_err.c \
   crypto/asn1/asn1_gen.c \
+  crypto/asn1/asn1_item_list.c \
   crypto/asn1/asn1_lib.c \
   crypto/asn1/asn1_par.c \
   crypto/asn1/asn_mime.c \
   crypto/asn1/asn_moid.c \
+  crypto/asn1/asn_mstbl.c \
   crypto/asn1/asn_pack.c \
   crypto/asn1/bio_asn1.c \
   crypto/asn1/bio_ndef.c \
   crypto/asn1/d2i_pr.c \
   crypto/asn1/d2i_pu.c \
   crypto/asn1/evp_asn1.c \
-  crypto/asn1/f_enum.c \
   crypto/asn1/f_int.c \
   crypto/asn1/f_string.c \
   crypto/asn1/i2d_pr.c \
@@ -67,52 +92,46 @@ LOCAL_SRC_FILES := \
   crypto/asn1/nsseq.c \
   crypto/asn1/p5_pbe.c \
   crypto/asn1/p5_pbev2.c \
+  crypto/asn1/p5_scrypt.c \
   crypto/asn1/p8_pkey.c \
   crypto/asn1/t_bitst.c \
-  crypto/asn1/t_crl.c \
   crypto/asn1/t_pkey.c \
-  crypto/asn1/t_req.c \
   crypto/asn1/t_spki.c \
-  crypto/asn1/t_x509.c \
-  crypto/asn1/t_x509a.c \
   crypto/asn1/tasn_dec.c \
   crypto/asn1/tasn_enc.c \
   crypto/asn1/tasn_fre.c \
   crypto/asn1/tasn_new.c \
   crypto/asn1/tasn_prn.c \
+  crypto/asn1/tasn_scn.c \
   crypto/asn1/tasn_typ.c \
   crypto/asn1/tasn_utl.c \
   crypto/asn1/x_algor.c \
-  crypto/asn1/x_attrib.c \
   crypto/asn1/x_bignum.c \
-  crypto/asn1/x_crl.c \
-  crypto/asn1/x_exten.c \
   crypto/asn1/x_info.c \
+  crypto/asn1/x_int64.c \
   crypto/asn1/x_long.c \
-  crypto/asn1/x_name.c \
-  crypto/asn1/x_nx509.c \
   crypto/asn1/x_pkey.c \
-  crypto/asn1/x_pubkey.c \
-  crypto/asn1/x_req.c \
   crypto/asn1/x_sig.c \
   crypto/asn1/x_spki.c \
   crypto/asn1/x_val.c \
-  crypto/asn1/x_x509.c \
-  crypto/asn1/x_x509a.c \
   crypto/bf/bf_cfb64.c \
   crypto/bf/bf_ecb.c \
   crypto/bf/bf_enc.c \
   crypto/bf/bf_ofb64.c \
   crypto/bf/bf_skey.c \
+  crypto/bio/b_addr.c \
   crypto/bio/b_dump.c \
   crypto/bio/b_print.c \
   crypto/bio/b_sock.c \
+  crypto/bio/b_sock2.c \
   crypto/bio/bf_buff.c \
+  crypto/bio/bf_lbuf.c \
   crypto/bio/bf_nbio.c \
   crypto/bio/bf_null.c \
   crypto/bio/bio_cb.c \
   crypto/bio/bio_err.c \
   crypto/bio/bio_lib.c \
+  crypto/bio/bio_meth.c \
   crypto/bio/bss_acpt.c \
   crypto/bio/bss_bio.c \
   crypto/bio/bss_conn.c \
@@ -128,12 +147,15 @@ LOCAL_SRC_FILES := \
   crypto/bn/bn_blind.c \
   crypto/bn/bn_const.c \
   crypto/bn/bn_ctx.c \
+  crypto/bn/bn_depr.c \
+  crypto/bn/bn_dh.c \
   crypto/bn/bn_div.c \
   crypto/bn/bn_err.c \
   crypto/bn/bn_exp.c \
   crypto/bn/bn_exp2.c \
   crypto/bn/bn_gcd.c \
   crypto/bn/bn_gf2m.c \
+  crypto/bn/bn_intern.c \
   crypto/bn/bn_kron.c \
   crypto/bn/bn_lib.c \
   crypto/bn/bn_mod.c \
@@ -148,9 +170,11 @@ LOCAL_SRC_FILES := \
   crypto/bn/bn_shift.c \
   crypto/bn/bn_sqr.c \
   crypto/bn/bn_sqrt.c \
+  crypto/bn/bn_srp.c \
   crypto/bn/bn_word.c \
+  crypto/bn/bn_x931p.c \
+  crypto/bn/rsaz_exp.c \
   crypto/buffer/buf_err.c \
-  crypto/buffer/buf_str.c \
   crypto/buffer/buffer.c \
   crypto/cmac/cm_ameth.c \
   crypto/cmac/cm_pmeth.c \
@@ -164,11 +188,11 @@ LOCAL_SRC_FILES := \
   crypto/cms/cms_err.c \
   crypto/cms/cms_ess.c \
   crypto/cms/cms_io.c \
+  crypto/cms/cms_kari.c \
   crypto/cms/cms_lib.c \
   crypto/cms/cms_pwri.c \
   crypto/cms/cms_sd.c \
   crypto/cms/cms_smime.c \
-  crypto/comp/c_rle.c \
   crypto/comp/c_zlib.c \
   crypto/comp/comp_err.c \
   crypto/comp/comp_lib.c \
@@ -179,32 +203,24 @@ LOCAL_SRC_FILES := \
   crypto/conf/conf_mall.c \
   crypto/conf/conf_mod.c \
   crypto/conf/conf_sap.c \
-  crypto/cpt_err.c \
-  crypto/cryptlib.c \
-  crypto/cversion.c \
+  crypto/conf/conf_ssl.c \
   crypto/des/cbc_cksm.c \
   crypto/des/cbc_enc.c \
   crypto/des/cfb64ede.c \
   crypto/des/cfb64enc.c \
   crypto/des/cfb_enc.c \
   crypto/des/des_enc.c \
-  crypto/des/des_old.c \
-  crypto/des/des_old2.c \
   crypto/des/ecb3_enc.c \
   crypto/des/ecb_enc.c \
-  crypto/des/ede_cbcm_enc.c \
-  crypto/des/enc_read.c \
-  crypto/des/enc_writ.c \
   crypto/des/fcrypt.c \
   crypto/des/fcrypt_b.c \
+  crypto/des/ncbc_enc.c \
   crypto/des/ofb64ede.c \
   crypto/des/ofb64enc.c \
   crypto/des/ofb_enc.c \
   crypto/des/pcbc_enc.c \
   crypto/des/qud_cksm.c \
   crypto/des/rand_key.c \
-  crypto/des/read2pwd.c \
-  crypto/des/rpc_enc.c \
   crypto/des/set_key.c \
   crypto/des/str2key.c \
   crypto/des/xcbc_enc.c \
@@ -214,9 +230,14 @@ LOCAL_SRC_FILES := \
   crypto/dh/dh_depr.c \
   crypto/dh/dh_err.c \
   crypto/dh/dh_gen.c \
+  crypto/dh/dh_kdf.c \
   crypto/dh/dh_key.c \
   crypto/dh/dh_lib.c \
+  crypto/dh/dh_meth.c \
   crypto/dh/dh_pmeth.c \
+  crypto/dh/dh_prn.c \
+  crypto/dh/dh_rfc5114.c \
+  crypto/dh/dh_rfc7919.c \
   crypto/dsa/dsa_ameth.c \
   crypto/dsa/dsa_asn1.c \
   crypto/dsa/dsa_depr.c \
@@ -224,6 +245,7 @@ LOCAL_SRC_FILES := \
   crypto/dsa/dsa_gen.c \
   crypto/dsa/dsa_key.c \
   crypto/dsa/dsa_lib.c \
+  crypto/dsa/dsa_meth.c \
   crypto/dsa/dsa_ossl.c \
   crypto/dsa/dsa_pmeth.c \
   crypto/dsa/dsa_prn.c \
@@ -233,10 +255,10 @@ LOCAL_SRC_FILES := \
   crypto/dso/dso_dlfcn.c \
   crypto/dso/dso_err.c \
   crypto/dso/dso_lib.c \
-  crypto/dso/dso_null.c \
   crypto/dso/dso_openssl.c \
-  crypto/ebcdic.c \
-  crypto/ec/ec2_mult.c \
+  crypto/dso/dso_vms.c \
+  crypto/dso/dso_win32.c \
+  crypto/ec/curve25519.c \
   crypto/ec/ec2_oct.c \
   crypto/ec/ec2_smpl.c \
   crypto/ec/ec_ameth.c \
@@ -246,73 +268,81 @@ LOCAL_SRC_FILES := \
   crypto/ec/ec_cvt.c \
   crypto/ec/ec_err.c \
   crypto/ec/ec_key.c \
+  crypto/ec/ec_kmeth.c \
   crypto/ec/ec_lib.c \
   crypto/ec/ec_mult.c \
   crypto/ec/ec_oct.c \
   crypto/ec/ec_pmeth.c \
   crypto/ec/ec_print.c \
+  crypto/ec/ecdh_kdf.c \
+  crypto/ec/ecdh_ossl.c \
+  crypto/ec/ecdsa_ossl.c \
+  crypto/ec/ecdsa_sign.c \
+  crypto/ec/ecdsa_vrf.c \
   crypto/ec/eck_prn.c \
   crypto/ec/ecp_mont.c \
   crypto/ec/ecp_nist.c \
+  crypto/ec/ecp_nistp224.c \
+  crypto/ec/ecp_nistp256.c \
+  crypto/ec/ecp_nistp521.c \
+  crypto/ec/ecp_nistputil.c \
+  crypto/ec/ecp_nistz256.c \
+  crypto/ec/ecp_nistz256_table.c \
   crypto/ec/ecp_oct.c \
   crypto/ec/ecp_smpl.c \
-  crypto/ecdh/ech_err.c \
-  crypto/ecdh/ech_key.c \
-  crypto/ecdh/ech_lib.c \
-  crypto/ecdh/ech_ossl.c \
-  crypto/ecdsa/ecs_asn1.c \
-  crypto/ecdsa/ecs_err.c \
-  crypto/ecdsa/ecs_lib.c \
-  crypto/ecdsa/ecs_ossl.c \
-  crypto/ecdsa/ecs_sign.c \
-  crypto/ecdsa/ecs_vrf.c \
+  crypto/ec/ecx_meth.c \
   crypto/engine/eng_all.c \
   crypto/engine/eng_cnf.c \
   crypto/engine/eng_ctrl.c \
+  crypto/engine/eng_devcrypto.c \
   crypto/engine/eng_dyn.c \
   crypto/engine/eng_err.c \
   crypto/engine/eng_fat.c \
   crypto/engine/eng_init.c \
   crypto/engine/eng_lib.c \
   crypto/engine/eng_list.c \
+  crypto/engine/eng_openssl.c \
   crypto/engine/eng_pkey.c \
+  crypto/engine/eng_rdrand.c \
   crypto/engine/eng_table.c \
   crypto/engine/tb_asnmth.c \
   crypto/engine/tb_cipher.c \
   crypto/engine/tb_dh.c \
   crypto/engine/tb_digest.c \
   crypto/engine/tb_dsa.c \
-  crypto/engine/tb_ecdh.c \
-  crypto/engine/tb_ecdsa.c \
+  crypto/engine/tb_eckey.c \
   crypto/engine/tb_pkmeth.c \
   crypto/engine/tb_rand.c \
   crypto/engine/tb_rsa.c \
-  crypto/engine/tb_store.c \
-  crypto/err/err.c \
-  crypto/err/err_all.c \
-  crypto/err/err_prn.c \
   crypto/evp/bio_b64.c \
   crypto/evp/bio_enc.c \
   crypto/evp/bio_md.c \
   crypto/evp/bio_ok.c \
-  crypto/evp/c_all.c \
   crypto/evp/c_allc.c \
   crypto/evp/c_alld.c \
+  crypto/evp/cmeth_lib.c \
   crypto/evp/digest.c \
   crypto/evp/e_aes.c \
   crypto/evp/e_aes_cbc_hmac_sha1.c \
+  crypto/evp/e_aes_cbc_hmac_sha256.c \
+  crypto/evp/e_aria.c \
   crypto/evp/e_bf.c \
+  crypto/evp/e_camellia.c \
+  crypto/evp/e_cast.c \
+  crypto/evp/e_chacha20_poly1305.c \
   crypto/evp/e_des.c \
   crypto/evp/e_des3.c \
+  crypto/evp/e_idea.c \
   crypto/evp/e_null.c \
   crypto/evp/e_old.c \
   crypto/evp/e_rc2.c \
   crypto/evp/e_rc4.c \
   crypto/evp/e_rc4_hmac_md5.c \
   crypto/evp/e_rc5.c \
+  crypto/evp/e_seed.c \
+  crypto/evp/e_sm4.c \
   crypto/evp/e_xcbc_d.c \
   crypto/evp/encode.c \
-  crypto/evp/evp_acnf.c \
   crypto/evp/evp_cnf.c \
   crypto/evp/evp_enc.c \
   crypto/evp/evp_err.c \
@@ -320,14 +350,15 @@ LOCAL_SRC_FILES := \
   crypto/evp/evp_lib.c \
   crypto/evp/evp_pbe.c \
   crypto/evp/evp_pkey.c \
-  crypto/evp/m_dss.c \
-  crypto/evp/m_dss1.c \
-  crypto/evp/m_ecdsa.c \
+  crypto/evp/m_md2.c \
   crypto/evp/m_md4.c \
   crypto/evp/m_md5.c \
+  crypto/evp/m_md5_sha1.c \
   crypto/evp/m_mdc2.c \
   crypto/evp/m_null.c \
+  crypto/evp/m_ripemd.c \
   crypto/evp/m_sha1.c \
+  crypto/evp/m_sha3.c \
   crypto/evp/m_sigver.c \
   crypto/evp/m_wp.c \
   crypto/evp/names.c \
@@ -340,34 +371,29 @@ LOCAL_SRC_FILES := \
   crypto/evp/p_seal.c \
   crypto/evp/p_sign.c \
   crypto/evp/p_verify.c \
+  crypto/evp/pbe_scrypt.c \
   crypto/evp/pmeth_fn.c \
   crypto/evp/pmeth_gn.c \
   crypto/evp/pmeth_lib.c \
-  crypto/ex_data.c \
   crypto/hmac/hm_ameth.c \
   crypto/hmac/hm_pmeth.c \
   crypto/hmac/hmac.c \
-  crypto/krb5/krb5_asn.c \
   crypto/lhash/lh_stats.c \
   crypto/lhash/lhash.c \
   crypto/md4/md4_dgst.c \
   crypto/md4/md4_one.c \
   crypto/md5/md5_dgst.c \
   crypto/md5/md5_one.c \
-  crypto/mem.c \
-  crypto/mem_clr.c \
-  crypto/mem_dbg.c \
   crypto/modes/cbc128.c \
   crypto/modes/ccm128.c \
   crypto/modes/cfb128.c \
   crypto/modes/ctr128.c \
+  crypto/modes/cts128.c \
   crypto/modes/gcm128.c \
+  crypto/modes/ocb128.c \
   crypto/modes/ofb128.c \
+  crypto/modes/wrap128.c \
   crypto/modes/xts128.c \
-  crypto/o_dir.c \
-  crypto/o_init.c \
-  crypto/o_str.c \
-  crypto/o_time.c \
   crypto/objects/o_names.c \
   crypto/objects/obj_dat.c \
   crypto/objects/obj_err.c \
@@ -382,6 +408,7 @@ LOCAL_SRC_FILES := \
   crypto/ocsp/ocsp_prn.c \
   crypto/ocsp/ocsp_srv.c \
   crypto/ocsp/ocsp_vfy.c \
+  crypto/ocsp/v3_ocsp.c \
   crypto/pem/pem_all.c \
   crypto/pem/pem_err.c \
   crypto/pem/pem_info.c \
@@ -389,7 +416,6 @@ LOCAL_SRC_FILES := \
   crypto/pem/pem_oth.c \
   crypto/pem/pem_pk8.c \
   crypto/pem/pem_pkey.c \
-  crypto/pem/pem_seal.c \
   crypto/pem/pem_sign.c \
   crypto/pem/pem_x509.c \
   crypto/pem/pem_xaux.c \
@@ -407,8 +433,10 @@ LOCAL_SRC_FILES := \
   crypto/pkcs12/p12_npas.c \
   crypto/pkcs12/p12_p8d.c \
   crypto/pkcs12/p12_p8e.c \
+  crypto/pkcs12/p12_sbag.c \
   crypto/pkcs12/p12_utl.c \
   crypto/pkcs12/pk12err.c \
+  crypto/pkcs7/bio_pk7.c \
   crypto/pkcs7/pk7_asn1.c \
   crypto/pkcs7/pk7_attr.c \
   crypto/pkcs7/pk7_doit.c \
@@ -416,33 +444,28 @@ LOCAL_SRC_FILES := \
   crypto/pkcs7/pk7_mime.c \
   crypto/pkcs7/pk7_smime.c \
   crypto/pkcs7/pkcs7err.c \
-  crypto/pqueue/pqueue.c \
-  crypto/rand/md_rand.c \
+  crypto/rand/drbg_ctr.c \
+  crypto/rand/drbg_lib.c \
   crypto/rand/rand_egd.c \
   crypto/rand/rand_err.c \
   crypto/rand/rand_lib.c \
   crypto/rand/rand_unix.c \
+  crypto/rand/rand_vms.c \
   crypto/rand/rand_win.c \
   crypto/rand/randfile.c \
-  crypto/rc2/rc2_cbc.c \
-  crypto/rc2/rc2_ecb.c \
-  crypto/rc2/rc2_skey.c \
-  crypto/rc2/rc2cfb64.c \
-  crypto/rc2/rc2ofb64.c \
-  crypto/rc4/rc4_enc.c \
-  crypto/rc4/rc4_skey.c \
-  crypto/rc4/rc4_utl.c \
   crypto/rsa/rsa_ameth.c \
   crypto/rsa/rsa_asn1.c \
   crypto/rsa/rsa_chk.c \
   crypto/rsa/rsa_crpt.c \
-  crypto/rsa/rsa_eay.c \
+  crypto/rsa/rsa_depr.c \
   crypto/rsa/rsa_err.c \
   crypto/rsa/rsa_gen.c \
   crypto/rsa/rsa_lib.c \
+  crypto/rsa/rsa_meth.c \
+  crypto/rsa/rsa_mp.c \
   crypto/rsa/rsa_none.c \
-  crypto/rsa/rsa_null.c \
   crypto/rsa/rsa_oaep.c \
+  crypto/rsa/rsa_ossl.c \
   crypto/rsa/rsa_pk1.c \
   crypto/rsa/rsa_pmeth.c \
   crypto/rsa/rsa_prn.c \
@@ -451,24 +474,37 @@ LOCAL_SRC_FILES := \
   crypto/rsa/rsa_sign.c \
   crypto/rsa/rsa_ssl.c \
   crypto/rsa/rsa_x931.c \
+  crypto/rsa/rsa_x931g.c \
+  crypto/sha/keccak1600.c \
   crypto/sha/sha1_one.c \
   crypto/sha/sha1dgst.c \
   crypto/sha/sha256.c \
   crypto/sha/sha512.c \
-  crypto/sha/sha_dgst.c \
   crypto/srp/srp_lib.c \
   crypto/srp/srp_vfy.c \
   crypto/stack/stack.c \
+  crypto/ts/ts_asn1.c \
+  crypto/ts/ts_conf.c \
   crypto/ts/ts_err.c \
+  crypto/ts/ts_lib.c \
+  crypto/ts/ts_req_print.c \
+  crypto/ts/ts_req_utils.c \
+  crypto/ts/ts_rsp_print.c \
+  crypto/ts/ts_rsp_sign.c \
+  crypto/ts/ts_rsp_utils.c \
+  crypto/ts/ts_rsp_verify.c \
+  crypto/ts/ts_verify_ctx.c \
   crypto/txt_db/txt_db.c \
-  crypto/ui/ui_compat.c \
   crypto/ui/ui_err.c \
   crypto/ui/ui_lib.c \
+  crypto/ui/ui_null.c \
   crypto/ui/ui_openssl.c \
   crypto/ui/ui_util.c \
-  crypto/uid.c \
   crypto/x509/by_dir.c \
   crypto/x509/by_file.c \
+  crypto/x509/t_crl.c \
+  crypto/x509/t_req.c \
+  crypto/x509/t_x509.c \
   crypto/x509/x509_att.c \
   crypto/x509/x509_cmp.c \
   crypto/x509/x509_d2.c \
@@ -476,6 +512,7 @@ LOCAL_SRC_FILES := \
   crypto/x509/x509_err.c \
   crypto/x509/x509_ext.c \
   crypto/x509/x509_lu.c \
+  crypto/x509/x509_meth.c \
   crypto/x509/x509_obj.c \
   crypto/x509/x509_r2x.c \
   crypto/x509/x509_req.c \
@@ -491,15 +528,26 @@ LOCAL_SRC_FILES := \
   crypto/x509/x509spki.c \
   crypto/x509/x509type.c \
   crypto/x509/x_all.c \
+  crypto/x509/x_attrib.c \
+  crypto/x509/x_crl.c \
+  crypto/x509/x_exten.c \
+  crypto/x509/x_name.c \
+  crypto/x509/x_pubkey.c \
+  crypto/x509/x_req.c \
+  crypto/x509/x_x509.c \
+  crypto/x509/x_x509a.c \
   crypto/x509v3/pcy_cache.c \
   crypto/x509v3/pcy_data.c \
   crypto/x509v3/pcy_lib.c \
   crypto/x509v3/pcy_map.c \
   crypto/x509v3/pcy_node.c \
   crypto/x509v3/pcy_tree.c \
+  crypto/x509v3/v3_addr.c \
+  crypto/x509v3/v3_admis.c \
   crypto/x509v3/v3_akey.c \
   crypto/x509v3/v3_akeya.c \
   crypto/x509v3/v3_alt.c \
+  crypto/x509v3/v3_asid.c \
   crypto/x509v3/v3_bcons.c \
   crypto/x509v3/v3_bitst.c \
   crypto/x509v3/v3_conf.c \
@@ -513,7 +561,6 @@ LOCAL_SRC_FILES := \
   crypto/x509v3/v3_int.c \
   crypto/x509v3/v3_lib.c \
   crypto/x509v3/v3_ncons.c \
-  crypto/x509v3/v3_ocsp.c \
   crypto/x509v3/v3_pci.c \
   crypto/x509v3/v3_pcia.c \
   crypto/x509v3/v3_pcons.c \
@@ -523,6 +570,7 @@ LOCAL_SRC_FILES := \
   crypto/x509v3/v3_purp.c \
   crypto/x509v3/v3_skey.c \
   crypto/x509v3/v3_sxnet.c \
+  crypto/x509v3/v3_tlsf.c \
   crypto/x509v3/v3_utl.c \
   crypto/x509v3/v3err.c
 
