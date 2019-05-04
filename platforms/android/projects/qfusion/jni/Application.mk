f0afcf6a812b54c7fe5c832864ffb305fc9ec182
Victor Luchits <vluchits@gmail.com>
2019-05-04 13:47:41 +0300

    Try C++14 for Android

--- platforms/android/projects/qfusion/jni/Application.mk
+++ platforms/android/projects/qfusion/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI := armeabi-v7a x86
 APP_CFLAGS := -ffast-math -fno-strict-aliasing -funroll-loops -std=gnu99 -Werror=return-type -Wno-typedef-redefinition
-APP_CPPFLAGS := -fexceptions -frtti -std=gnu++11
+APP_CPPFLAGS := -fexceptions -frtti -std=gnu++14
 APP_OPTIM := release
 APP_PLATFORM := android-16
 APP_STL := c++_shared
