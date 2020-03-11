33b21bd5900c05a4444cc3b56d4bb33b87e77945
Victor Luchits <vluchits@gmail.com>
2020-03-11 12:08:58 +0300

    RmlUi third-party containers test

--- platforms/android/projects/qfusion/jni/Application.mk
+++ platforms/android/projects/qfusion/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_ABI := armeabi-v7a x86
-APP_CFLAGS := -ffast-math -fno-strict-aliasing -funroll-loops -std=gnu99 -Werror=return-type -Wno-typedef-redefinition -DRMLUI_STATIC_LIB=1 -DRMLUI_NO_FONT_INTERFACE_DEFAULT=1 -DRMLUI_NO_THIRDPARTY_CONTAINERS=1 -DMINIZ_NO_STDIO=1 -DMINIZ_NO_ARCHIVE_APIS=1 -DMINIZ_NO_ARCHIVE_WRITING_APIS=1
+APP_CFLAGS := -ffast-math -fno-strict-aliasing -funroll-loops -std=gnu99 -Werror=return-type -Wno-typedef-redefinition -DRMLUI_STATIC_LIB=1 -DRMLUI_NO_FONT_INTERFACE_DEFAULT=1 -DMINIZ_NO_STDIO=1 -DMINIZ_NO_ARCHIVE_APIS=1 -DMINIZ_NO_ARCHIVE_WRITING_APIS=1 -D__STDC_LIMIT_MACROS -D__STDC_CONSTANT_MACROS
 APP_CPPFLAGS := -fexceptions -frtti -std=gnu++14
 APP_OPTIM := release
 APP_PLATFORM := android-18
