57bc7ff7ec4a88386c517d565093abda31b9c0f8
Michael Pollind <mpollind@gmail.com>
2026-03-05 19:51:26 -0800

    feat: fix packaging for darwin (#502)
    * feat: fix packaging for darwin
    
    Signed-off-by: Michael Pollind <michaelpollind@Michaels-Mini.lan>
    
    * feat: add package status message
    
    Signed-off-by: Michael Pollind <mpollind@gmail.com>
    
    * feat: fix deployment path for osx
    
    Signed-off-by: Michael Pollind <mpollind@gmail.com>
    
    ---------
    
    Signed-off-by: Michael Pollind <michaelpollind@Michaels-Mini.lan>
    Signed-off-by: Michael Pollind <mpollind@gmail.com>
    Co-authored-by: Michael Pollind <michaelpollind@Michaels-Mini.lan>

--- source/package_assets.cmake
+++ source/package_assets.cmake
@@ -1,3 +1,6 @@
+
+message(STATUS "Binary Directory: ${BIN_DIR}/basewf")
+
 if (${CMAKE_SYSTEM_NAME} MATCHES "Windows")
   execute_process (COMMAND cmd /c "7z a ${BIN_DIR}/basewf/data0_000_21.zip -r" WORKING_DIRECTORY "${ASSET_ROOT}/data0_000_21/")
   execute_process (COMMAND cmd /c "7z a ${BIN_DIR}/basewf/data0_000_21pure.zip -r"  WORKING_DIRECTORY "${ASSET_ROOT}/data0_000_21pure/")
